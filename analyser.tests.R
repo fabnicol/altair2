5372c7bc69eedebebd43e3a859e4cad7216b9c48
Fabrice Nicol <fabrnicol@noos.fr>
2014-03-24 17:32:39 +0100

    ajout de analyser.tests.R et biblio....R

---
+++ analyser.tests.R
@@ -0,0 +1,88 @@
+analyser.tests <- function ()
+{
+  attach(Bdp.ldp, warn.conflicts=FALSE)
+  
+  # Tests du type : incompatibilités entre indemnités et statut
+  
+  # NBI pour non titulaires
+  
+  NBI.aux.non.titulaires <- Bdp.ldp[ ! Statut %in% c("TITULAIRE","STAGIAIRE") & as.character(Code) %in% codes.NBI, colonnes.sélectionnées]
+  
+  nombre.de.ldp.NBI.nontit <- nrow(NBI.aux.non.titulaires)
+  
+  # heures sup >= 25h/mois
+  
+  HS.sup.25 <- with(Bdp, Bdp[Heures.Sup. >= 25 , c("Matricule", "Statut", "Mois", "Heures.Sup.", "Brut")])
+  
+  nombre.de.ldp.HS.sup.25 <- nrow(HS.sup.25)
+  
+  # Prime de fonctions informatiques : pas dans la base de VLB
+  # on cherche la chaine de char. "INFO" dans les libellés de primes
+  # variante : filtre <- regexpr(".*(INFO|PFI|P.F.I).*", toupper(Bdp.ldp$Libellé)) et regmatches(Bdp.ldp$Libellé, filtre)
+  
+  filtre<-grep(".*(INFO|PFI|P.F.I).*", Libellé, ignore.case=TRUE)
+  
+  personnels.prime.informatique <- Bdp.ldp[ filtre, colonnes.sélectionnées]
+  
+  primes.informatiques.potentielles<-unique(Libellé[filtre])
+  
+  nombre.de.ldp.personnels.pfi <- nrow(personnels.prime.informatique)
+  
+  # Vacations et statut de fonctionnaire
+  
+  ldp.fonctionnaires.et.vacations <- Bdp.ldp[ Statut %in% c("TITULAIRE", "STAGIAIRE") & est.code.de.type("VACATIONS"), colonnes.sélectionnées]
+  
+  nombre.de.ldp.fonctionnaires.et.vacations <- nrow(ldp.fonctionnaires.et.vacations)
+  
+  # Vacations et régime indemnitaire
+  
+  Bdp.ldp.vacations <- Bdp.ldp[est.code.de.type("VACATIONS"), colonnes.sélectionnées]
+  
+  matricules.nontit.et.vacations <- unique(Bdp.ldp.vacations[ ! Bdp.ldp.vacations$Statut %in% c("TITULAIRE", "STAGIAIRE"), "Matricule"])
+  
+  vacations.concernées <- Bdp.ldp.vacations[Matricule %in% matricules.nontit.et.vacations, ]
+  
+  RI.et.vacations <- Bdp.ldp[ Matricule %in% matricules.nontit.et.vacations & est.code.de.type("INDEMNITAIRE.OU.CONTRACTUEL"), colonnes.sélectionnées]
+  
+  traitement.et.vacations <- Bdp.ldp[ Matricule %in% matricules.nontit.et.vacations & est.code.de.type("TRAITEMENT"), colonnes.sélectionnées]
+  
+  nombre.de.ldp.RI.et.vacations <- nrow(RI.et.vacations)
+  nombre.de.ldp.traitement.et.vacations <- nrow(traitement.et.vacations)
+  
+  #IFTS et IB >= 380 (IM >= 350)
+  
+  filtre.iat<-grep(".*(IAT|I.A.T|.*Adm.*Tech).*", Libellé, ignore.case=TRUE)
+  filtre.ifts<-grep(".*(IFTS|I.F.T.S|.*FORF.*TRAV.*SUPP).*", Libellé, ignore.case=TRUE)
+  codes.ifts <- unique(Bdp.ldp[filtre.ifts, "Code"])
+  
+  sélection.matricules <- intersect(!duplicated(Bdp.ldp[ Indice < 350, c("Matricule")]), !duplicated( Bdp.ldp[ filtre.ifts, c("Matricule")]))
+  
+  Bdp.ldp[Matricule %in% sélection.matricules & Code %in% codes.ifts & (Indice < 350 ), colonnes.sélectionnées]
+  
+  rm(sélection.matricules)
+  
+  # IFTS et non tit
+  
+  ifts.et.non.tit <- Bdp.ldp[Code %in% codes.ifts & !Statut %in% c("TITULAIRE", "STAGIAIRE"), colonnes.sélectionnées]
+  
+  # Incomptabilités de primes entre elles
+  
+  #IAT et IFTS
+  
+  personnels.iat.ifts <- intersect(as.character(Bdp.ldp[ filtre.iat, c("Matricule")]), as.character(Bdp.ldp[ filtre.ifts, c("Matricule")]))
+  
+  nb.personnels.iat.ifts <- length(personnels.iat.ifts)
+  
+  # Primes entre elles : 
+  
+  pretty.print(nombre.de.ldp.NBI.nontit)
+  pretty.print(nombre.de.ldp.HS.sup.25)
+  pretty.print(primes.informatiques.potentielles)
+  pretty.print(nombre.de.ldp.personnels.pfi)
+  pretty.print(nombre.de.ldp.fonctionnaires.et.vacations)
+  pretty.print(nombre.de.ldp.RI.et.vacations)
+  pretty.print(nombre.de.ldp.traitement.et.vacations)
+  pretty.print(nb.personnels.iat.ifts)
+  
+  detach(Bdp.ldp)
+}
---
+++ bibliotheque.fonctions.paie.R
@@ -0,0 +1,75 @@
+#----------------------------------------------------------------------------------------------------------------------
+##
+#  Fonctions auxiliaires
+##
+
+# Trouve le numéro de la ligne à laquelle se situe la liste des noms de variables
+# en recherchant soit le mot "Matricule" soit une expression du type "Code..."
+# Il faudra déduire ce "skip" du read.csv2 pour récupérer proprement les noms de variable
+
+# Pour cela on scanne les 25 premières lignes de la table une première fois
+
+
+scan.prime <- function(texte)
+{
+	unique(Bdp.ldp[grep(paste0(".*(", texte,").*"), Bdp.ldp$Libellé, ignore.case=TRUE), c("Matricule", "Libellé", "Libellé")])
+}
+
+trouver.valeur.skip <-  function(chemin.table) 
+  max(
+    sapply(
+      read.csv2(chemin.table, nrows=25),
+      function(x) 
+      {
+        m <- match("Matricule", x, nomatch=0 ) 
+        if (m == 0)
+          m <- pmatch("Code", x, nomatch=0, duplicates.ok=FALSE ) 
+        return(m)
+      }
+    ))
+
+
+selectionner.cle.matricule <-  function(Base1, Base2) 
+  subset(Base1, 
+         select=c(champ.detection.1, setdiff(names(Base1),names(Base2))))
+
+selectionner.cle.matricule.mois <-  function(Base1, Base2) 
+  subset(Base1, 
+         select=c(champ.detection.1,champ.detection.2,
+                  setdiff(names(Base1),names(Base2))))
+
+
+chemin <-  function(fichier) 
+  file.path(chemin.dossier, fichier)
+
+read.csv.skip <- function(x) 
+  {
+    chem <- chemin(x)
+    read.csv2(chem, skip=trouver.valeur.skip(chem))
+  }
+
+sauv.base.univarié <- function(x)  write.csv2(x, paste0(chemin(deparse(substitute(x))), ".csv"), row.names=FALSE)
+
+sauv.base <- function(...) 
+  {
+    tmp <- as.list(match.call()) 
+    tmp[1] <- NULL
+    lapply(tmp, sauv.base.univarié)
+    return(0)
+  }
+
+# Utiliser une assignation globale 
+# car la fonction anonyme ne comporte que de variables locales
+
+Read.csv <- function(vect.chemin) 
+{
+  Read.csv.result <- data.frame(NULL)
+  Vectorize(function(x) Read.csv.result <<- rbind(Read.csv.result, read.csv.skip(x)))(vect.chemin)
+  Read.csv.result
+}
+
+pretty.print <- function(x) cat(gsub(".", " ",deparse(substitute(x)), fixed=TRUE), "   ", x,"\n")
+
+est.code.de.type <- function(x) Bdp.ldp$Code %in% Code.prime[Code.prime$Type.rémunération == x, "Code"]
+
+#-----------------------------------------------------------------------------------------------------------------
