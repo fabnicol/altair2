e87cb31cbd374a17fd70eea71dda79352b0abb07
Fabrice Nicol <fabrnicol@gmail.com>
2014-05-24 15:58:28 +0200

    fusion par nom, prénom

--- Tests/Exemple/prologue.R
+++ Tests/Exemple/prologue.R
@@ -1,16 +1,7 @@
 # prologue 
 # doit être dans le même répertoire que le programme principal et sa bibliothèque
 
-windows <- (Sys.info()["sysname"] != "Linux")
-
-if (windows)
-{ 
-    encodage.entrée <- "ISO-8859-1"    
-} else 
-{
-    encodage.entrée <- "UTF-8"    
-}
-
+fusionner.nom.prénom <- TRUE
 
 charger.bases <- TRUE
 sauvegarder.bases <- TRUE
@@ -37,3 +28,20 @@ fin.période.sous.revue   <- 2011
 champ.détection.1 <- étiquette.matricule
 champ.détection.2 <- "Code"
 date.format <- "%d/%m/%Y"
+
+ifelse(fusionner.nom.prénom, 
+       clé.fusion <<- c("Nom", "Prénom"),
+       clé.fusion <<- étiquette.matricule)
+
+if (!setOSWindows)
+{
+  windows <- (Sys.info()["sysname"] != "Linux")
+  
+  if (windows)
+  { 
+    encodage.entrée <- "ISO-8859-1"    
+  } else 
+  {
+    encodage.entrée <- "UTF-8"    
+  }
+}
