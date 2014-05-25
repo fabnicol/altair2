8d248041a62ae9f7c1a26a1aaa1028c3e2f88305
Fabrice Nicol <fabrnicol@gmail.com>
2014-05-25 12:57:08 +0200

    import experimental

--- Tests/Exemple/prologue.R
+++ Tests/Exemple/prologue.R
@@ -3,31 +3,29 @@
 
 fusionner.nom.prénom <- TRUE
 
-charger.bases <- TRUE
-sauvegarder.bases <- FALSE
-générer.codes <- FALSE
+    charger.bases <- TRUE
+sauvegarder.bases <- TRUE
+    générer.codes <- FALSE
 tester.matricules <- FALSE
+calculer.nb.jours <- TRUE
 
 nom.fichier.paie  <- "PEV CA-Lignes de paye"
 nom.bulletin.paie <- "PEV CA-Bulletins de paye"
 
-chemin.dossier <- getwd()
-
-chemin.dossier.bases <- paste0(chemin.dossier, "/Bases")
-
+        chemin.dossier <- getwd()
+  chemin.dossier.bases <- paste0(chemin.dossier, "/Bases")
 chemin.dossier.données <- paste0(chemin.dossier, "/Donnees")
-
   
-début.période.sous.revue <- 2010
-fin.période.sous.revue   <- 2011
-étiquette.matricule <- "Matricule"
+   début.période.sous.revue <- 2010
+   fin.période.sous.revue   <- 2011
+        étiquette.matricule <- "Matricule"
 étiquette.Type.rémunération <- "Type rémunération"
-étiquette.année <- "Année"
-étiquette.libellé <- "Libellé"
-étiquette.rém.indemn <- "Rémunération contractuelle ou indemnitaire"
-champ.détection.1 <- étiquette.matricule
-champ.détection.2 <- "Code"
-date.format <- "%d/%m/%Y"
+            étiquette.année <- "Année"
+          étiquette.libellé <- "Libellé"
+       étiquette.rém.indemn <- "Rémunération contractuelle ou indemnitaire"
+          champ.détection.1 <- étiquette.matricule
+          champ.détection.2 <- "Code"
+                date.format <- "%d/%m/%Y"
 
 ifelse(fusionner.nom.prénom, 
        clé.fusion <<- c("Nom", "Prénom"),
