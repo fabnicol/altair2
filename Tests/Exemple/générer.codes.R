98a50b2421eae8185a6d6d40b1cd4a1450803d5d
Fabrice Nicol <fabrnicol@gmail.com>
2014-10-23 18:54:45 +0200

    heuristics
    Former-commit-id: 4e36d64c361ec3380a00369bf046ef8f7a098ce2 [formerly 0c5b17ed95c33d0d54988e52ff78e3f5ff304bf0]
    Former-commit-id: 4c487f42d8dad32930b9e34485b2f9685bd3ff26

--- Tests/Exemple/générer.codes.R
+++ Tests/Exemple/générer.codes.R
@@ -1,42 +1,50 @@
-message("Génération de la base des codes de paie et des libellés.")
-
-if (table.rapide) {
+générer.base.codes <- function(B) {
+  message("Génération de la base des codes de paie et des libellés.")
   
-  codes.paiement.généré <- unique(Lignes.paie[ , c(étiquette.code, étiquette.libellé), with = FALSE])
+  if (table.rapide) {
+    
+    codes.paiement.généré <<- unique(B[ , c(étiquette.code, étiquette.libellé, "Type"), with = FALSE])
+    
+  } else {
+    
+    codes.paiement.généré <<- unique(B[ , c(étiquette.code, étiquette.libellé)])
+  }
   
-} else {
+  codes.paiement.généré <<- cbind(codes.paiement.généré[mixedorder(codes.paiement.généré$Code), ], character(nrow(codes.paiement.généré)))
   
-  codes.paiement.généré <- unique(Lignes.paie[ , c(étiquette.code, étiquette.libellé)])
+  if (!import.direct) 
+    names(codes.paiement.généré)[3] <- étiquette.Type.rémunération
+  else
+    names(codes.paiement.généré)[4] <- "Hors Brut"
+  
+  class(codes.paiement.généré) <- "data.frame"
+  codes.paiement.généré$Libellé <- iconv(codes.paiement.généré$Libellé, from = "UTF-8", to=encodage.entrée, mark = FALSE)
+  nom.fichier.codes <- racine %+% "codes.paiement.généré"
+  Sauv.base(chemin.dossier.données, "codes.paiement.généré", nom.fichier.codes)
+  
+  #'---
+  #'
+  #'# Tableau des codes de paiement
+  #'
+  #'##  Renseigner le type de rémunération
+  #'
+  #'Utiliser les codes : TRAITEMENT, INDEMNITAIRE, ELU, VACATIONS, AUTRES
+  #'
+  
+  kable(codes.paiement.généré, row.names = FALSE)
+  
+  #'
+  
+  
+  if (file.exists(file.path(chemin.dossier.données, nom.fichier.codes %+% ".csv")))
+    message("Génération des codes : voir fichier Données/" %+% nom.fichier.codes %+% ".csv")  else
+      message("Les codes n'ont pas été générés.")
+  
+  
+  message("Le programme est arrêté après génération de la base de codes et libellés.
+  Relancer Altair après avoir renseigné la troisième colonne
+  et placé le fichier sous le répertoire Données avec le nom " %+% nom.fichier.codes.paiement)
+  
+  if (générer.codes) stop("Codes générés", call. = FALSE, domain = NULL)
+  
 }
-
-codes.paiement.généré <- cbind(codes.paiement.généré[mixedorder(codes.paiement.généré$Code), ], character(nrow(codes.paiement.généré)))
-
-names(codes.paiement.généré)[3] <- étiquette.Type.rémunération
-class(codes.paiement.généré) <- "data.frame"
-codes.paiement.généré$Libellé <- iconv(codes.paiement.généré$Libellé, from = "UTF-8", to=encodage.entrée, mark = FALSE)
-nom.fichier.codes <- racine %+% "codes.paiement.généré"
-Sauv.base(chemin.dossier.données, "codes.paiement.généré", nom.fichier.codes)
-
-#'---
-#'
-#'# Tableau des codes de paiement
-#'
-#'##  Renseigner le type de rémunération
-#'
-#'Utiliser les codes : TRAITEMENT, INDEMNITAIRE, ELU, VACATIONS, AUTRES
-#'
-
-kable(codes.paiement.généré, row.names = FALSE)
-
-#'
-
-
-if (file.exists(file.path(chemin.dossier.données, nom.fichier.codes %+% ".csv")))
-  message("Génération des codes : voir fichier Données/" %+% nom.fichier.codes %+% ".csv")  else
-    message("Les codes n'ont pas été générés.")
-
-
-message("Le programme est arrêté après génération de la base de codes et libellés.
-Relancer Altair après avoir renseigné la troisième colonne
-et placé le fichier sous le répertoire Données avec le nom " %+% nom.fichier.codes.paiement)
-browser()
