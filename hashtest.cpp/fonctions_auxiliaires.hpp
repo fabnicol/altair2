0bda163ecb5a3eb65840d33246c5ba3054d2fd30
Fab Nicol <fabnicol@users.sourceforge.net>
2014-10-26 23:04:58 +0100

    Add -T for table slicing by year or max line number
    Former-commit-id: 4d5ff007706672dba0bc5d8d2c803882edd5f580 [formerly 3e4d57fcd03ca8c0bc623d4777cd81dd1fc71e8c]
    Former-commit-id: 047051d33dd7e70c77a7908f84c4a10a840d297b

--- hashtest.cpp/fonctions_auxiliaires.hpp
+++ hashtest.cpp/fonctions_auxiliaires.hpp
@@ -8,6 +8,8 @@
 
 #include "validator.hpp"
 
-int lire_argument(int argc, const char* const c_str);
+int32_t lire_argument(int argc, const char* const c_str);
 int calculer_memoire_requise(info_t* info);
+FILE* ouvrir_fichier_base(info_t* info, int i);
+char* ecrire_chemin_base(char* chemin_base, int rang_fichier_base);
 #endif // FONCTIONS_AUXILIAIRES_HPP_INCLUDED
--- hashtest.cpp/table.cpp
+++ hashtest.cpp/table.cpp
@@ -3,32 +3,9 @@
 //#endif // __cplusplus
 
 #include "table.hpp"
+#include "fonctions_auxiliaires.hpp"
 
-uint64_t generer_table_bulletins(const char* chemin_base, info_t* Info)
-{
-    FILE* base = fopen(chemin_base, "wb");
-    fseek(base, 0, SEEK_SET);
-    if (base == NULL)
-    {
-        fprintf(stderr, "%s\n", "Impossible d'ouvrir le fichier de sortie.");
-        exit(-1000);
-    }
-    else
-        printf("%s\n", "Enregistrement de la base csv");
 
-    unsigned i;
-    for (i = 0; i < sizeof(entete_char)/sizeof(char*) -1; i++)
-        fprintf(base, "%s%c", entete_char[i], Info[0].separateur);
-
-    fprintf(base, "%s\n", entete_char[i]);
-
-    uint64_t compteur=0;
-
-    compteur = boucle_ecriture(base, Info);
-
-    fclose(base);
-    return compteur;
-}
 
 int64_t generer_table_standard(const char* chemin_table, info_t* info)
 {
@@ -39,10 +16,27 @@ int64_t generer_table_standard(const char* chemin_table, info_t* info)
 
 /* Il doit y avoir BESOIN_MEMOIRE_ENTETE + 6 champs plus Type, soit 18 + 6 +1 = 23 champs et 24 séparateurs + saut de ligne = 48 char + \0*/
 
+inline FILE* ouvrir_nouvelle_base(info_t* info, unsigned* rang_fichier_base, FILE* base)
+{
+    if (fclose(base) == EOF)
+    {
+        perror("Problème fermeture fichier base");
+        exit(-902);
+    }
+
+    if (*rang_fichier_base >= 1000)
+    {
+        fprintf(stderr, "%s", "Ne peut générer que 999 bases au plus\n");
+        exit(-904);
+    }
+
+    return ouvrir_fichier_base(info, ++*rang_fichier_base);
+}
+
 #define TAILLE_FORMAT (Info[0].minimum_memoire_p_ligne + 6 +1)*4
 #define VAR(X) Info[i].Table[agent][X]
 
-uint64_t boucle_ecriture(FILE* base, info_t* Info)
+void boucle_ecriture(info_t* Info)
 {
     int ligne = 0;
     uint64_t compteur = 0;
@@ -62,25 +56,47 @@ uint64_t boucle_ecriture(FILE* base, info_t* Info)
     format_base[TAILLE_FORMAT - 1] = '\0';
 
     char sep = Info[0].separateur;
+    char* annee_courante = (char*) Info[0].Table[0][Annee];
+    unsigned rang_fichier_base = 1;
 
     // Un peu low-level C, mais beaucoup plus rapide que de coder un fprintf pour chaque item.
     // Gain d'exécution : 30s pour fprintf par item
     //                    22s sur une ligne
+
+    FILE* base = ouvrir_fichier_base(Info, rang_fichier_base);
 
     for (int i = 0; i < Info[0].nbfil; i++)
     {
         for (uint32_t agent = 0; agent < Info[i].NCumAgentXml; agent++)
         {
             /* BOUCLER SUR L */
+
+            if (Info[i].taille_base == PAR_ANNEE && strcmp((const char*)VAR(Annee), annee_courante))
+            {
+                base = ouvrir_nouvelle_base(&Info[i], &rang_fichier_base, base);
+                fprintf(stderr, "Année : %s Table générée.\n", annee_courante);
+                annee_courante = (char*) VAR(Annee);
+            }
+
             unsigned l = Info[i].minimum_memoire_p_ligne;
             char* type =  (char*) type_remuneration_traduit[0];
 
             int allocation_memoire = (Info[i].reduire_consommation_memoire)?
-                                      Info[i].minimum_memoire_p_ligne + nbType + Info[i].NLigne[agent] * 6
-                                    : Info[i].minimum_memoire_p_ligne + nbType + Info[i].nbLigneUtilisateur * 6 * sizeof(xmlChar*);
+                                     Info[i].minimum_memoire_p_ligne + nbType + Info[i].NLigne[agent] * 6
+                                     : Info[i].minimum_memoire_p_ligne + nbType + Info[i].nbLigneUtilisateur * 6 * sizeof(xmlChar*);
 
             while (ligne < Info[i].NLigne[agent])
             {
+                if (Info[i].taille_base > MONOLITHIQUE && compteur + ligne == rang_fichier_base*Info[i].taille_base -1)
+                {
+                    fprintf(stderr, "Table n° %d de %d lignes générée, lignes %d à %d.\n",
+                            rang_fichier_base,
+                            Info[i].taille_base,
+                            (rang_fichier_base-1) * Info[i].taille_base +1,
+                            rang_fichier_base * Info[i].taille_base);
+                    base = ouvrir_nouvelle_base(&Info[i], &rang_fichier_base, base);
+                }
+
                 if (l + 6 == allocation_memoire)
                 {
                     perror("Max lignes de paye atteint !");
@@ -130,7 +146,27 @@ uint64_t boucle_ecriture(FILE* base, info_t* Info)
 
         if (i) Info[0].nbLigne += Info[i].nbLigne;
     }
-    return compteur;
+
+    if (base)
+    {
+        fclose(base);
+        switch (Info[0].taille_base)
+        {
+        case MONOLITHIQUE :
+            fprintf(stderr, "Table de %" PRIu64 " lignes générée.\n", compteur);
+            break;
+        case PAR_ANNEE    :
+            fprintf(stderr, "Année : %s Table générée.\n", annee_courante);
+            break;
+        default :  /* Taille définie par l'utilisateur */
+            fprintf(stderr, "Table n° %d de %" PRIu64 " lignes générée, lignes %d à %" PRIu64 ".\n",
+                    rang_fichier_base,
+                    compteur - (rang_fichier_base-1)*Info[0].taille_base,
+                    (rang_fichier_base-1)*Info[0].taille_base +1,
+                    compteur);
+
+        }
+    }
 }
 #undef VAR
 //#ifdef __cplusplus
