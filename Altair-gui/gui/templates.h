4fbe86c1396c0566cd7f85eb3a6a729a273e4aab
Fabrice Nicol <fabrnicol@gmail.com>
2014-10-31 13:09:27 +0100

    add ETP, add gui repo

---
+++ Altair-gui/gui/templates.h
@@ -0,0 +1,16 @@
+#ifndef TEMPLATES_H
+#define TEMPLATES_H
+#include <QHash>
+
+
+template <typename T, typename U> void createHash(QHash<T, U > *H, const QList<T> *L, const QList<U> *M)
+{
+    if ((H == nullptr) || (L == nullptr) || (M == nullptr)) return;
+    QListIterator<T> i(*L);
+    QListIterator<U> j(*M);
+
+    while ((j.hasNext()) && (i.hasNext()))
+        (*H)[i.next()]=j.next();
+}
+
+#endif // TEMPLATES_H
