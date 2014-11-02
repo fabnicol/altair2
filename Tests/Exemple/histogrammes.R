743e233c35a9102bf32162122f99d9dec85ec641
Fab Nicol <fabnicol@users.sourceforge.net>
2014-11-02 15:44:05 +0100

    add histogrammes.R
    Former-commit-id: 0e719372b2afcb3817f751d3c94d5c2a986dd5fc

---
+++ Tests/Exemple/histogrammes.R
@@ -0,0 +1,61 @@
+
+histogrammes <- function()  {
+
+if (longueur.non.na(filtre.fonctionnaire(Montant.brut.annuel) > 0)) 
+  hist(filtre.fonctionnaire(Montant.brut.annuel)/1000,
+       xlab = "En milliers d'euros  (< 40 k€)\n",
+       ylab = "Effectif",
+       xlim = c(0, 40),
+       main = "Rémunération brute annuelle des fonctionnaires en " %+% année,
+       col = "blue",
+       nclass = 50)
+
+#'
+if (longueur.non.na(filtre.fonctionnaire(Montant.brut.annuel[Montant.brut.annuel > 40000]) > 0)) 
+  hist(filtre.fonctionnaire(Montant.brut.annuel[Montant.brut.annuel > 40000])/1000,
+       xlab = "En milliers d'euros (> 40 k€)\n",
+       ylab = "Effectif",
+       xlim = c(40, 120),
+       main = "Rémunération brute annuelle des fonctionnaires en " %+% année,
+       col = "blue",
+       nclass = 50)
+
+#'
+if (longueur.non.na(filtre.fonctionnaire(rémunération.indemnitaire.imposable) > 0))
+  hist(filtre.fonctionnaire(rémunération.indemnitaire.imposable)/1000,
+       xlab = "En milliers d'euros (< 10 k€)\n",
+       ylab = "Effectif",
+       xlim = c(0, 10),
+       main = "Rémunération indemnitaire imposable annuelle des fonctionnaires en " %+% année,
+       col = "blue",
+       nclass = 80)
+
+if (filtre.fonctionnaire(rémunération.indemnitaire.imposable[rémunération.indemnitaire.imposable > 10000]) > 0)
+  hist(filtre.fonctionnaire(rémunération.indemnitaire.imposable[rémunération.indemnitaire.imposable > 10000])/1000,
+       xlab = "En milliers d'euros (> 10 k€)\n",
+       ylab = "Effectif",
+       xlim = c(10, 60),
+       main = "Rémunération indemnitaire imposable annuelle des fonctionnaires en " %+% année,
+       col = "blue",
+       nclass = 50)
+
+
+if (longueur.non.na(filtre.fonctionnaire(part.rémunération.indemnitaire) > 0)) 
+  hist(filtre.fonctionnaire(part.rémunération.indemnitaire),
+       xlab = "Part des indemnités imposables (< 30 %) dans la rémunération brute en %\n",
+       ylab = "Effectif",
+       main = "Part indemnitaire de la rémunération annuelle des fonctionnaires en " %+% année,
+       xlim = c(0,30),
+       col = "blue",
+       nclass = 60)
+
+if (longueur.non.na(filtre.fonctionnaire(part.rémunération.indemnitaire[part.rémunération.indemnitaire > 30]) > 0)) 
+  hist(filtre.fonctionnaire(part.rémunération.indemnitaire[part.rémunération.indemnitaire > 30]),
+       xlab = "Part des indemnités imposables (> 30 %)  dans la rémunération brute en %\n",
+       ylab = "Effectif",
+       main = "Part indemnitaire de la rémunération annuelle des fonctionnaires en " %+% année,
+       xlim = c(30,60),
+       col = "blue",
+       nclass = 60)
+
+}
