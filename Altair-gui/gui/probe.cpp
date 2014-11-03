9ae3ed3cc65eb9bb9e26a7e9cf26d0454b327155
Fab Nicol <fabnicol@users.sourceforge.net>
2014-11-03 02:42:56 +0100

    gui : remove off-by-one bug in tabs when removing first item R : many bugfixes

--- Altair-gui/gui/probe.cpp
+++ Altair-gui/gui/probe.cpp
@@ -1,52 +1,10 @@
 #include "probe.h"
 
 
-void StandardComplianceProbe::checkStandardCompliance()
-{
-
-//sampleSize=audioFileFormat.sampleSize();
-//sampleRate=audioFileFormat.sampleRate();
-//channelCount=audioFileFormat.channelCount();
-
-
-//if (((channelCount ==0) || ( sampleRate == 0) || (sampleSize == 0)) ||
-//    (channelCount > 6) ||
-//    ((sampleSize != 16) && (sampleSize != 24)))
-//  {
-//    decoderCompliance=isNonCompliant;
-//    return;
-//  }
-
-//switch (audioFileFormat.sampleRate())
-//{
-//case 96000:
-//case 48000: decoderCompliance=isStrictlyDVDVideoCompliant;
-//                      break;
-//case 44100:
-//case 88200:
-//case 176400:
-//case 192000:
-//                         decoderCompliance = isStrictlyDVDAudioCompliant;
-//                         break;
-//default:
-
-//  decoderCompliance=isNonCompliant;
-//}
-
-return;
-}
-
-bool StandardComplianceProbe::isStandardCompliant()
-{
-//    if (audioZone == VIDEO)
-//       return (decoderCompliance == isStrictlyDVDVideoCompliant) ;
-
-//    return (decoderCompliance == isStrictlyDVDAudioCompliant ||  decoderCompliance == isStrictlyDVDVideoCompliant);
-}
 
 void StandardComplianceProbe::getXhlCharacteristics(const QString &filename)
 {
-    QString sox_guessed_codec=QString();
+   
 
     decoderCompliance=xhlCharacteristics::isNonCompliant;   //default
 
@@ -59,7 +17,7 @@ void StandardComplianceProbe::getXhlCharacteristics(const QString &filename)
       {
 
         decoderCompliance=xhlCharacteristics::isCompliant;
-      //checkStandardCompliance();
+ 
       }
 }
 
