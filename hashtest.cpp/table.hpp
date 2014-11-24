186afb5439748b3ecb191fe53b3a12114517fed3
Fabrice Nicol Linux <fabrnicol@gmail.com>
2014-11-24 01:33:43 +0100

    renames

--- hashtest.cpp/table.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef TABLE_HPP_INCLUDED
-#define TABLE_HPP_INCLUDED
-#include "validator.hpp"
-void generer_table_bulletins(info_t* Info);
-int64_t generer_table_standard(const char* chemin_table, info_t* info);
-void boucle_ecriture(info_t* Info);
-
-#endif // TABLE_HPP_INCLUDED
