a7baca5b8a1c3caf9c37da758d599959a6da591f
Fabrice Nicol Linux <fabrnicol@gmail.com>
2014-11-24 01:21:59 +0100

    Add new folders and archive cleanup
    Former-commit-id: 5e0c8268b7b40e51c6c0157f00f913e25d88157b [formerly f5af1a252977b95412592054c704bb65ca7a78ea]
    Former-commit-id: 1df1ff534d42a56f82e68fe546f45befc96e7a4d

---
+++ Interface/build-altair-Desktop_Qt_5_3_GCC_64bit-Release/moc_altair.cpp
@@ -0,0 +1,324 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'altair.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.3.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../gui/altair.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'altair.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.3.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_Altair_t {
+    QByteArrayData data[28];
+    char stringdata[444];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Altair_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Altair_t qt_meta_stringdata_Altair = {
+    {
+QT_MOC_LITERAL(0, 0, 6),
+QT_MOC_LITERAL(1, 7, 21),
+QT_MOC_LITERAL(2, 29, 0),
+QT_MOC_LITERAL(3, 30, 13),
+QT_MOC_LITERAL(4, 44, 28),
+QT_MOC_LITERAL(5, 73, 27),
+QT_MOC_LITERAL(6, 101, 29),
+QT_MOC_LITERAL(7, 131, 21),
+QT_MOC_LITERAL(8, 153, 22),
+QT_MOC_LITERAL(9, 176, 15),
+QT_MOC_LITERAL(10, 192, 3),
+QT_MOC_LITERAL(11, 196, 10),
+QT_MOC_LITERAL(12, 207, 15),
+QT_MOC_LITERAL(13, 223, 4),
+QT_MOC_LITERAL(14, 228, 8),
+QT_MOC_LITERAL(15, 237, 8),
+QT_MOC_LITERAL(16, 246, 11),
+QT_MOC_LITERAL(17, 258, 11),
+QT_MOC_LITERAL(18, 270, 21),
+QT_MOC_LITERAL(19, 292, 26),
+QT_MOC_LITERAL(20, 319, 6),
+QT_MOC_LITERAL(21, 326, 18),
+QT_MOC_LITERAL(22, 345, 16),
+QT_MOC_LITERAL(23, 362, 16),
+QT_MOC_LITERAL(24, 379, 11),
+QT_MOC_LITERAL(25, 391, 15),
+QT_MOC_LITERAL(26, 407, 12),
+QT_MOC_LITERAL(27, 420, 23)
+    },
+    "Altair\0hasIndexChangedSignal\0\0"
+    "updateProject\0on_openProjectButton_clicked\0"
+    "on_moveUpItemButton_clicked\0"
+    "on_moveDownItemButton_clicked\0"
+    "on_deleteItem_clicked\0removeFileTreeElements\0"
+    "createDirectory\0run\0runRAltair\0"
+    "processFinished\0code\0exitCode\0addGroup\0"
+    "deleteGroup\0killProcess\0on_helpButton_clicked\0"
+    "on_xhlFilterButton_clicked\0active\0"
+    "requestSaveProject\0writeProjectFile\0"
+    "assignGroupFiles\0group_index\0"
+    "openProjectFile\0closeProject\0"
+    "on_switch_to_progress_2"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Altair[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      25,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    0,  139,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       3,    1,  140,    2, 0x0a /* Public */,
+       3,    0,  143,    2, 0x2a /* Public | MethodCloned */,
+       4,    0,  144,    2, 0x0a /* Public */,
+       5,    0,  145,    2, 0x08 /* Private */,
+       6,    0,  146,    2, 0x08 /* Private */,
+       7,    0,  147,    2, 0x08 /* Private */,
+       8,    0,  148,    2, 0x08 /* Private */,
+       9,    0,  149,    2, 0x08 /* Private */,
+      10,    0,  150,    2, 0x08 /* Private */,
+      11,    0,  151,    2, 0x08 /* Private */,
+      12,    1,  152,    2, 0x08 /* Private */,
+      12,    1,  155,    2, 0x08 /* Private */,
+      15,    0,  158,    2, 0x08 /* Private */,
+      16,    0,  159,    2, 0x08 /* Private */,
+      17,    0,  160,    2, 0x08 /* Private */,
+      18,    0,  161,    2, 0x08 /* Private */,
+      19,    1,  162,    2, 0x08 /* Private */,
+      19,    0,  165,    2, 0x28 /* Private | MethodCloned */,
+      21,    0,  166,    2, 0x08 /* Private */,
+      22,    0,  167,    2, 0x08 /* Private */,
+      23,    1,  168,    2, 0x08 /* Private */,
+      25,    0,  171,    2, 0x08 /* Private */,
+      26,    0,  172,    2, 0x08 /* Private */,
+      27,    0,  173,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::Bool,    2,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   13,
+    QMetaType::Void, 0x80000000 | 14,    2,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Bool,   20,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   24,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void Altair::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        Altair *_t = static_cast<Altair *>(_o);
+        switch (_id) {
+        case 0: _t->hasIndexChangedSignal(); break;
+        case 1: _t->updateProject((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 2: _t->updateProject(); break;
+        case 3: _t->on_openProjectButton_clicked(); break;
+        case 4: _t->on_moveUpItemButton_clicked(); break;
+        case 5: _t->on_moveDownItemButton_clicked(); break;
+        case 6: _t->on_deleteItem_clicked(); break;
+        case 7: _t->removeFileTreeElements(); break;
+        case 8: _t->createDirectory(); break;
+        case 9: _t->run(); break;
+        case 10: _t->runRAltair(); break;
+        case 11: _t->processFinished((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 12: _t->processFinished((*reinterpret_cast< exitCode(*)>(_a[1]))); break;
+        case 13: _t->addGroup(); break;
+        case 14: _t->deleteGroup(); break;
+        case 15: _t->killProcess(); break;
+        case 16: _t->on_helpButton_clicked(); break;
+        case 17: _t->on_xhlFilterButton_clicked((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 18: _t->on_xhlFilterButton_clicked(); break;
+        case 19: _t->requestSaveProject(); break;
+        case 20: _t->writeProjectFile(); break;
+        case 21: _t->assignGroupFiles((*reinterpret_cast< const int(*)>(_a[1]))); break;
+        case 22: _t->openProjectFile(); break;
+        case 23: _t->closeProject(); break;
+        case 24: _t->on_switch_to_progress_2(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        void **func = reinterpret_cast<void **>(_a[1]);
+        {
+            typedef void (Altair::*_t)();
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&Altair::hasIndexChangedSignal)) {
+                *result = 0;
+            }
+        }
+    }
+}
+
+const QMetaObject Altair::staticMetaObject = {
+    { &common::staticMetaObject, qt_meta_stringdata_Altair.data,
+      qt_meta_data_Altair,  qt_static_metacall, 0, 0}
+};
+
+
+const QMetaObject *Altair::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Altair::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_Altair.stringdata))
+        return static_cast<void*>(const_cast< Altair*>(this));
+    return common::qt_metacast(_clname);
+}
+
+int Altair::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = common::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 25)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 25;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 25)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 25;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void Altair::hasIndexChangedSignal()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, 0);
+}
+struct qt_meta_stringdata_FProgressBar_t {
+    QByteArrayData data[3];
+    char stringdata[19];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_FProgressBar_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_FProgressBar_t qt_meta_stringdata_FProgressBar = {
+    {
+QT_MOC_LITERAL(0, 0, 12),
+QT_MOC_LITERAL(1, 13, 4),
+QT_MOC_LITERAL(2, 18, 0)
+    },
+    "FProgressBar\0stop\0"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_FProgressBar[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       1,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   19,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void FProgressBar::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        FProgressBar *_t = static_cast<FProgressBar *>(_o);
+        switch (_id) {
+        case 0: _t->stop(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+const QMetaObject FProgressBar::staticMetaObject = {
+    { &QWidget::staticMetaObject, qt_meta_stringdata_FProgressBar.data,
+      qt_meta_data_FProgressBar,  qt_static_metacall, 0, 0}
+};
+
+
+const QMetaObject *FProgressBar::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *FProgressBar::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_FProgressBar.stringdata))
+        return static_cast<void*>(const_cast< FProgressBar*>(this));
+    return QWidget::qt_metacast(_clname);
+}
+
+int FProgressBar::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 1)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 1;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
---
+++ Interface/gui/probe.h
@@ -0,0 +1,37 @@
+#ifndef PROBE_H
+#define PROBE_H
+#include "common.h"
+
+class StandardComplianceProbe : public QObject
+{
+    Q_OBJECT
+
+private:
+
+    QStringList xhlFormatList;
+
+    uint audioZone;
+
+    void getXhlCharacteristics(const QString &filename);
+    enum class xhlCharacteristics   { isCompliant, isNonCompliant};
+    xhlCharacteristics decoderCompliance;
+    int nAgent=0;
+    int char2=0;
+    int char3=0;
+
+ public:
+    StandardComplianceProbe(QString &filename, uint zone=0)
+    {
+        audioZone=zone;
+        getXhlCharacteristics(filename);
+        xhlFormatList << "xhl";
+    }
+
+    QString getnAgent() {return QString::number(nAgent);}
+    QString getChar2() {return QString::number(char2);}
+    QString getChar3() {return QString::number(char3);}
+
+};
+
+
+#endif // PROBE_H
