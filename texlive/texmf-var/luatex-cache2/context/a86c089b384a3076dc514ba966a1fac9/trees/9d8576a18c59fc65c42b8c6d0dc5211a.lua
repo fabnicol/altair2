b82f2df4c5a1015f64e926c717fbb1438bab8f94
Fabrice Nicol <fabrnicol@gmail.com>
2015-02-04 16:00:51 +0100

    move to texlive

---
+++ texlive/texmf-var/luatex-cache2/context/a86c089b384a3076dc514ba966a1fac9/trees/9d8576a18c59fc65c42b8c6d0dc5211a.lua
@@ -0,0 +1,16 @@
+return {
+ ["content"]={
+  ["__directories__"]=0,
+  ["__files__"]=1,
+  ["__path__"]="selfautoparent:texmf-config",
+  ["__remappings__"]=1,
+  ["ls-R"]="",
+  ["remap:ls-r"]="ls-R",
+ },
+ ["date"]="2015-02-04",
+ ["root"]="selfautoparent:texmf-config",
+ ["time"]="14:01:00",
+ ["type"]="files",
+ ["uuid"]="68cb9083-4a0c-96ab-09b9-a6e9276bc90b",
+ ["version"]="1.0.1",
+}
