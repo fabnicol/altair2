0a673a48f29a5d5bd1babaae5d058d05eeae43fb
fabrice nicol <fabrnicol@gmail.com>
2015-09-14 19:33:20 +0200

    add contextmenu

--- Interface/gui/mainwindow.cpp
+++ Interface/gui/mainwindow.cpp
@@ -42,8 +42,8 @@ MainWindow::MainWindow(char* projectName)
   setCentralWidget(altair);
 
   altair->addActions(actionList);
-  altair->setContextMenuPolicy(Qt::ActionsContextMenu);
-  altair->setContextMenuPolicy(Qt::ActionsContextMenu);
+  //altair->setContextMenuPolicy(Qt::ActionsContextMenu);
+
 
   bottomDockWidget=new QDockWidget;
   bottomTabWidget=new QTabWidget;
