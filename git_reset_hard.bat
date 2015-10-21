b8992b1ca425a9a06d647fa028ac5a0d5a3e0c0b
Fabrice Nicol <fabrnicol@gmail.com>
2015-10-21 17:53:47 +0200

    modded git reset.bat and build directory

--- git_reset_hard.bat
+++ git_reset_hard.bat
@@ -1,2 +1,7 @@
 @echo off
-c:\windows\system32\cmd.exe /C  echo Extraction du code source... && Git\bin\git.exe reset --hard master 
+c:\windows\system32\cmd.exe /C  echo Sauvegarde du code source. Cette sauvegarde peut durer entre 15 et 30 minutes...\
+&& Git\bin\git.exe init \
+&& Git\bin\git.exe add . \
+&& Git\bin\git.exe config --global user.email unknown@unknown.fr \
+&& Git\bin\git.exe config --global user.name  "Prénom Nom" \
+&& Git\bin\git.exe commit -am "Start"  
