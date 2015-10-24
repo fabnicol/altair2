bd8fadc6ccd6c5914c75bd887a9436abda2333d5
PrÚnom Nom <unknown@unknown.fr>
2015-10-24 15:00:00 +0200

    refresh rdata.table and add script to build automùatically from github + move RTools --> RBuildTools using symlink to recycle mingw64-5.2 as a compiler, avoiding compiler duplication
    Former-commit-id: c9efa0e24ba3fe4cf00f57b1e2242673919e9807 [formerly 40176a7ec2b371dd93c10838e150d15a9a56b40b]
    Former-commit-id: be7a39f4055e28aff03f44d6458e8961eed8f679

---
+++ Tests/Exemple/build.rdata.table.R
@@ -0,0 +1,27 @@
+
+rtoolspath <- "RBuildTools/3.3/bin"
+
+wd <- getwd()
+if (grepl("altair$", wd, ignore.case=TRUE) == FALSE)
+{
+  stop("le répertoire de travail doit être .../altair")  
+}
+
+.libPaths("lib")
+
+localPath <- Sys.getenv("PATH")
+
+library(devtools)
+
+# No need to use find_rtools() (broken or just useless here)
+
+if (grepl(rtoolspath, localPath, ignore.case=TRUE) == FALSE) {
+
+  Sys.setenv(PATH = paste(localPath, file.path(wd, "..", rtoolspath), sep=";"))
+  
+  install_github("Rdatatable/data.table", build_vignettes = FALSE)
+}
+
+# Restoring previous system path to avoid cluttering
+
+Sys.setenv(PATH = localPath)
