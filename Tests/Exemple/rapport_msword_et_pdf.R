bd1b4a2a720baaeb75885bbd2db9e8ab460f8d1b
PrÚnom Nom <unknown@unknown.fr>
2015-11-21 23:23:45 +0100

    renewed pdf-and-word script
    Former-commit-id: bae61e2c44fde202b19bff14e76d185774c510bf [formerly 01dfad58d57999122a0cafc1c61499d08cc3fd85]
    Former-commit-id: 0d2ff13f2b9f257324bad81a33b91e7b8abc429b

--- Tests/Exemple/rapport_msword_et_pdf.R
+++ Tests/Exemple/rapport_msword_et_pdf.R
@@ -1,10 +1,10 @@
+# encoder ce script en windows-1252
 encodage.code.source <- "ISO-8859-1"
 initwd <- getwd()
-if (corriger.environnement.système) {
-  
-  invisible(Sys.setenv(PATH = paste0(Sys.getenv("PATH"), Sys.getenv("R_HOME"), "../texlive/miktex/bin/x64;")))
-  
-}
+
+invisible(Sys.setenv(PATH = paste0(Sys.getenv("PATH"), ";", file.path(Sys.getenv("R_HOME"), "../texlive/miktex/bin;"))))
+
+
 setwd(file.path(Sys.getenv("R_HOME"), "../Tests/Exemple"))
 source("syspaths.R", encoding=encodage.code.source)
 source("prologue.R", encoding=encodage.code.source)
@@ -18,8 +18,9 @@ library(knitr)
 spin("altair.ansi.R")
 writeLines(iconv(readLines("altair.ansi.md"), from = "WINDOWS-1252", to = "UTF-8"), "altair.utf8.md", useBytes=TRUE)
 
-system(paste(file.path(Sys.getenv("R_HOME"), "../RStudio/bin/pandoc/pandoc.exe"), "-V papersize=A4 +RTS -K512m -RTS altair.utf8.md --to latex --from markdown+autolink_bare_uris+ascii_identifiers+tex_math_single_backslash-implicit_figures --output altair.pdf --template ", file.path(Sys.getenv("R_HOME"), "..", "lib/rmarkdown/rmd/latex/default.tex"), " --highlight-style tango --latex-engine ", file.path(Sys.getenv("R_HOME"), "..", "texlive/miktex/bin/x64/pdflatex.exe"), " --variable geometry:margin=1in" ))
+system(paste(file.path(Sys.getenv("R_HOME"), "../RStudio/bin/pandoc/pandoc.exe"), "-V papersize=A4 +RTS -K512m -RTS altair.utf8.md --to latex --from markdown+autolink_bare_uris+ascii_identifiers+tex_math_single_backslash-implicit_figures --output altair.pdf --template ", file.path(Sys.getenv("R_HOME"), "..", "lib/rmarkdown/rmd/latex/default.tex"), " --highlight-style tango --latex-engine ", file.path(Sys.getenv("R_HOME"), "..", "texlive/miktex/bin/pdflatex.exe"), " --variable geometry:margin=1in" ))
 shell("start acrord32.exe altair.pdf")
+
 
 system(paste(file.path(Sys.getenv("R_HOME"), "../RStudio/bin/pandoc/pandoc.exe"), "altair.utf8.md --to docx --from markdown+autolink_bare_uris+ascii_identifiers+tex_math_single_backslash-implicit_figures --output altaïr.docx --highlight-style tango")) 
 shell("start winword altaïr.docx")
