aad847332b0eb4635f0f933fb28d7b8d4ed48c1c
Fabrice Nicol <fabrnicol@gmail.com>
2015-11-24 19:44:43 +0100

    linux adjustments : to do: fix trema issue + rapport script adjustments
    Former-commit-id: a32b63d5f9f630b6c4a2160c90d561d4114c7738 [formerly 4e49a865cba6a72ebcc4a8343c79dd255f4d9c35]
    Former-commit-id: 2684aead154b7497a90dd8d89180292013ffa88a

--- Tests/Exemple/rapport_msword.R
+++ Tests/Exemple/rapport_msword.R
@@ -3,6 +3,7 @@ encodage.code.source <- "ISO-8859-1"
 initwd <- getwd()
 
 setwd(file.path(Sys.getenv("R_HOME"), "../Tests/Exemple"))
+setwd("Tests/Exemple")
 source("syspaths.R", encoding=encodage.code.source)
 source("prologue.R", encoding=encodage.code.source)
 
@@ -14,9 +15,13 @@ writeLines(iconv(readLines("altair.R"), from = encodage.code.source, to = "WINDO
 library(knitr)
 spin("altair.ansi.R")
 writeLines(iconv(readLines("altair.ansi.md"), from = "WINDOWS-1252", to = "UTF-8"), "altair.utf8.md", useBytes=TRUE)
-
-system(paste(file.path(Sys.getenv("R_HOME"), "../RStudio/bin/pandoc/pandoc.exe"), "altair.utf8.md --to docx --from markdown+autolink_bare_uris+ascii_identifiers+tex_math_single_backslash-implicit_figures --output altaïr.docx --highlight-style tango")) 
-shell("start winword altaïr.docx")
+if (setOSWindows) {
+  system(paste(file.path(Sys.getenv("R_HOME"), "../RStudio/bin/pandoc/pandoc.exe"), "altair.utf8.md --to docx --from markdown+autolink_bare_uris+ascii_identifiers+tex_math_single_backslash-implicit_figures --output altaïr.docx --highlight-style tango")) 
+  shell("start winword altaïr.docx")
+} else {
+  system(paste("/usr/bin/pandoc.exe", "altair.utf8.md --to odt --from markdown+autolink_bare_uris+ascii_identifiers+tex_math_single_backslash-implicit_figures --output altaïr.odt --highlight-style tango")) 
+  shell("/usr/bin/lowriter altaïr.odt")
+}
 
 setwd(initwd)
 
