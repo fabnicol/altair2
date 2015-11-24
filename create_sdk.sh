387b09cd8c90ff74242e4536eaf1ccd8e80ad16a
PrÚnom Nom <unknown@unknown.fr>
2015-11-24 03:24:51 +0100

    platform adjustments

--- create_sdk.sh
+++ create_sdk.sh
@@ -8,7 +8,7 @@ then
 fi
 mkdir -p Altaïr-SDK/altair
 var=0
-for term in LICENCE dlls Docs Git Interface Interface_windows Interface_windows_min lib Local Outils R R64 Roaming RStudio RStudio-project RBuildTools texlive win  ".Rproj.user" 
+for term in LICENCE dlls Docs Git Interface Interface_windows Interface_windows_min lib Local Local.dev Outils R R64 Roaming Roaming.dev RStudio RStudio-project RBuildTools texlive win  ".Rproj.user" 
 do
 	((var++))
 	echo -ne "$var/20: copie de $term en cours...               "\\r
