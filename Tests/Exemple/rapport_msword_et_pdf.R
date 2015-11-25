8a8fd3a8daaf50a652c9de5cb83e8a84b440327e
Fabrice Nicol <fabrnicol@gmail.com>
2015-11-25 14:50:55 +0100

    finalized linux pdf versions
    Former-commit-id: 9367e69ef479ce8b62320854205afbe35073da8c [formerly d2f0f1d23735ef6ccd3f4f33e7efd605f5d0fa2b]
    Former-commit-id: bf49cc5b17a51e8f3c98be07f01e64febb6590e0

--- Tests/Exemple/rapport_msword_et_pdf.R
+++ Tests/Exemple/rapport_msword_et_pdf.R
@@ -2,27 +2,35 @@
 encodage.code.source <- "ISO-8859-1"
 initwd <- getwd()
 
-invisible(Sys.setenv(PATH = paste0(Sys.getenv("PATH"), ";", file.path(Sys.getenv("R_HOME"), "../texlive/miktex/bin;"))))
+
+if (setOSWindows) {
+  setwd(file.path(Sys.getenv("R_HOME"), "../Tests/Exemple"))
+  source("syspaths.R", encoding=encodage.code.source)
+  source("prologue.R", encoding=encodage.code.source)
+  writeLines(iconv(readLines("altair.R"), from = encodage.code.source, to = "WINDOWS-1252"), "altair.ansi.R")  
+  library(knitr)
+  spin("altair.ansi.R")
+  writeLines(iconv(readLines("altair.ansi.md"), from = "WINDOWS-1252", to = "UTF-8"), "altair.utf8.md", useBytes=TRUE)
+  system(paste(file.path(Sys.getenv("R_HOME"), "../RStudio/bin/pandoc/pandoc.exe"), "altair.utf8.md --to docx --from markdown+autolink_bare_uris+ascii_identifiers+tex_math_single_backslash-implicit_figures --output altaïr.docx --highlight-style tango")) 
+  shell("start winword altaïr.docx")
+  
+  system(paste(file.path(Sys.getenv("R_HOME"), "../RStudio/bin/pandoc/pandoc.exe"), "-V papersize=A4 +RTS -K512m -RTS altair.utf8.md --to latex --from markdown+autolink_bare_uris+ascii_identifiers+tex_math_single_backslash-implicit_figures --output altaïr.pdf --template ", file.path(Sys.getenv("R_HOME"), "..", "lib/rmarkdown/rmd/latex/default.tex"), " --highlight-style tango --latex-engine ", file.path(Sys.getenv("R_HOME"), "..", "texlive/miktex/bin/pdflatex.exe"), " --variable geometry:margin=1in" ))
+  shell("start acrord32.exe altaïr.pdf")
+  
+} else {
+  
+  setwd("Tests/Exemple")
+  source("syspaths.R", encoding=encodage.code.source)
+  source("prologue.R", encoding=encodage.code.source)
+  writeLines(iconv(readLines("altair.R"), from = encodage.code.source, to = "UTF-8"), "altair.utf8.R")  
+  library(knitr)
+  spin("altair.utf8.R")
+  system(paste("/usr/bin/pandoc", "altair.utf8.md --to odt --from markdown+autolink_bare_uris+ascii_identifiers+tex_math_single_backslash-implicit_figures --output altaïr.odt --highlight-style tango")) 
+  system("/usr/bin/lowriter altaïr.odt")
+  system(paste("/usr/bin/pandoc", "altair.utf8.md -V papersize=A4 +RTS -K512m -RTS altair.utf8.md --to latex --from markdown+autolink_bare_uris+ascii_identifiers+tex_math_single_backslash-implicit_figures --output altaïr.pdf --template ../../lib_linux/rmarkdown/rmd/latex/default.tex --highlight-style tango --latex-engine pdflatex --variable geometry:margin=1in"))
+  system("/usr/bin/okular altaïr.pdf")
+}
 
 
-setwd(file.path(Sys.getenv("R_HOME"), "../Tests/Exemple"))
-source("syspaths.R", encoding=encodage.code.source)
-source("prologue.R", encoding=encodage.code.source)
-
-library_path <- if (setOSWindows) "lib" else "lib_linux"
-
-.libPaths(file.path(Sys.getenv("R_HOME"), "..", library_path))
-
-writeLines(iconv(readLines("altair.R"), from = encodage.code.source, to = "WINDOWS-1252"), "altair.ansi.R")
-library(knitr)
-spin("altair.ansi.R")
-writeLines(iconv(readLines("altair.ansi.md"), from = "WINDOWS-1252", to = "UTF-8"), "altair.utf8.md", useBytes=TRUE)
-
-system(paste(file.path(Sys.getenv("R_HOME"), "../RStudio/bin/pandoc/pandoc.exe"), "-V papersize=A4 +RTS -K512m -RTS altair.utf8.md --to latex --from markdown+autolink_bare_uris+ascii_identifiers+tex_math_single_backslash-implicit_figures --output altaïr.pdf --template ", file.path(Sys.getenv("R_HOME"), "..", "lib/rmarkdown/rmd/latex/default.tex"), " --highlight-style tango --latex-engine ", file.path(Sys.getenv("R_HOME"), "..", "texlive/miktex/bin/pdflatex.exe"), " --variable geometry:margin=1in" ))
-shell("start acrord32.exe altaïr.pdf")
-
-
-system(paste(file.path(Sys.getenv("R_HOME"), "../RStudio/bin/pandoc/pandoc.exe"), "altair.utf8.md --to docx --from markdown+autolink_bare_uris+ascii_identifiers+tex_math_single_backslash-implicit_figures --output altaïr.docx --highlight-style tango")) 
-shell("start winword altaïr.docx")
 setwd(initwd)
 
