e882e9d2e21616fdeeffc16f2098e5e99c632025
PrÚnom Nom <unknown@unknown.fr>
2015-11-30 23:25:58 +0100

    add finaliser_distribution

---
+++ finaliser_distribution.bat
@@ -0,0 +1,2 @@
+xcopy /I /Y /E C:\Users\Public\Altair\Roaming\RStudio  %USERPROFILE%\AppData\Roaming\RStudio
+xcopy /I /Y /E C:\Users\Public\Altair\Local\RStudio-desktop  %USERPROFILE%\AppData\Local\RStudio-desktop
