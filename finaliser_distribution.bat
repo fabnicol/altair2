8fbf06a0f95f16d43190f286d71bef045a207680
Fabrice Nicol <fabrnicol@gmail.com>
2015-12-01 12:24:38 +0100

    finaliser_distribution.bat renewed

--- finaliser_distribution.bat
+++ finaliser_distribution.bat
@@ -2,6 +2,6 @@ xcopy /I /Y /E C:\Users\Public\Altair\Roaming\RStudio  %USERPROFILE%\AppData\Roa
 xcopy /I /Y /E C:\Users\Public\Altair\Local\RStudio-desktop  %USERPROFILE%\AppData\Local\RStudio-desktop
 set courant=%~dp0
 cd %courant%
-%courant%\Git\bin\git.exe rm -f  --cached Tests\Exemple\Donnees\R-Altaïr\Bulletins.csv
+%courant%\Git\bin\git.exe rm -rf  --cached Tests\Exemple\Donnees\*
 %courant%\Git\bin\git.exe rm -rf --cached .Rproj.user
 %courant%\Git\bin\git.exe rm -f  --cached lhx\cl
