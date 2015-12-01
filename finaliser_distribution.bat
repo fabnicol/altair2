4fad0323b7736b890a996d78e1393a9af14010bd
Fabrice Nicol <fabrnicol@gmail.com>
2015-12-01 14:26:54 +0100

    distro mods
    Former-commit-id: 63c846d5d7ffc90d7569fded04c917cb49bf2cb2 [formerly 7c66a10f3361626c94ab2f6238e0f86526ba82a5]
    Former-commit-id: c302903bf1222ea2eb30a585dabacdd8336057a1

--- finaliser_distribution.bat
+++ finaliser_distribution.bat
@@ -1,2 +1,7 @@
 xcopy /I /Y /E C:\Users\Public\Altair\Roaming\RStudio  %USERPROFILE%\AppData\Roaming\RStudio
 xcopy /I /Y /E C:\Users\Public\Altair\Local\RStudio-desktop  %USERPROFILE%\AppData\Local\RStudio-desktop
+set courant=%~dp0
+cd %courant%
+%courant%\Git\bin\git.exe rm -rf  --cached Tests\Exemple\Donnees\*
+%courant%\Git\bin\git.exe rm -rf --cached .Rproj.user
+%courant%\Git\bin\git.exe rm -f  --cached lhx\cl
