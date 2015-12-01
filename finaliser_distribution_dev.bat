cf4cdb3d62316cd1e54e9e4c61ec47575f5c1f79
Fabrice Nicol <fabrnicol@gmail.com>
2015-12-01 18:30:45 +0100

    modded finaliser_distribution_dev
    Former-commit-id: d83cd134c6ee72c30cf83b2def751f15941da74f [formerly a041b0b5238bfbe4282a61dd84ff5c2c903e7c9c]
    Former-commit-id: 1dd98eaff83a4f98e9105b514ec71ef0c3b824d7

--- finaliser_distribution_dev.bat
+++ finaliser_distribution_dev.bat
@@ -1,9 +1,23 @@
+@echo off
+echo Actualisation de la copie locale de l'entrepot 
 set courant=%~dp0
 cd %courant%
+%courant%\Git\bin\git.exe --version
+set /p login="Entrez votre login de session : "
+set /p password="Entrez votre mot de passe de session : "
+set /p proxy="Entrez le proxy [P pour 10.163.251.190]: "
+set /p port="Entrez le port [P pour 8080]: "
+if "%proxy%" ==  "P" set proxy=10.163.251.190
+if "%port%"  ==  "P" set port=8080
+copy /Y %courant%\Git\etc\gitconfig %courant%\Git\etc\gitconfig.save
+echo [https] >> %courant%\Git\etc\gitconfig
+echo     proxy = %login%:%password%@%proxy%:%port% >> %courant%\Git\etc\gitconfig
 %courant%\Git\bin\git.exe reset --hard HEAD
 %courant%\Git\bin\git.exe clean -d -f
 %courant%\Git\bin\git.exe remote add -t distribution-dev origin https:\\github.com\fabnicol\altair.git
 %courant%\Git\bin\git.exe fetch origin distribution-dev
 %courant%\Git\bin\git.exe merge -s recursive -X theirs FETCH_HEAD
+del %courant%\Git\etc\gitconfig
+copy /Y %courant%\Git\etc\gitconfig.save %courant%\Git\etc\gitconfig
 
 
