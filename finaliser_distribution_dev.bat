5b8bae22c4615ce386898c3ba04346fc2ddd48c0
PrÚnom Nom <unknown@unknown.fr>
2015-12-01 19:47:56 +0100

    new git config mods for dev distro
    Former-commit-id: 4f46b9b2dd669a98d2c27f538d22ba89593f8e17 [formerly bc921a9d8cbe5115d10c3341c612ea6aee380bb6]
    Former-commit-id: bd0ba4ef9b2699248a37a463aaf9ca9091a56562

--- finaliser_distribution_dev.bat
+++ finaliser_distribution_dev.bat
@@ -12,9 +12,9 @@ if "%port%"  ==  "P" set port=8080
 copy /Y %courant%\Git\etc\gitconfig %courant%\Git\etc\gitconfig.save
 echo [https] >> %courant%\Git\etc\gitconfig
 echo     proxy = %login%:%password%@%proxy%:%port% >> %courant%\Git\etc\gitconfig
-%courant%\Git\bin\git.exe reset --hard HEAD
+REM %courant%\Git\bin\git.exe reset --hard HEAD
 %courant%\Git\bin\git.exe clean -d -f
-%courant%\Git\bin\git.exe remote add -t distribution-dev origin https:\\github.com\fabnicol\altair.git
+%courant%\Git\bin\git.exe remote add -t distribution-dev origin https://github.com/fabnicol/altair.git
 %courant%\Git\bin\git.exe fetch origin distribution-dev
 %courant%\Git\bin\git.exe merge -s recursive -X theirs FETCH_HEAD
 del %courant%\Git\etc\gitconfig
