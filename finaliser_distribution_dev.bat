9eab0ae71175b4e002893daf7eafea83c3a2d1e6
Fabrice Nicol <fabrnicol@gmail.com>
2015-12-02 01:04:47 +0100

    added category analysis to PFR, dynamic pay changes and IFTS checks on INM 350

--- finaliser_distribution_dev.bat
+++ finaliser_distribution_dev.bat
@@ -3,6 +3,7 @@ echo Actualisation de la copie locale de l'entrepot
 set courant=%~dp0
 cd %courant%
 %courant%\Git\bin\git.exe --version
+%courant%\Git\bin\git.exe reset --hard HEAD
 set /p login="Entrez votre login de session : "
 set /p password="Entrez votre mot de passe de session : "
 set /p proxy="Entrez le proxy [P pour 10.163.251.190]: "
@@ -12,8 +13,7 @@ if "%port%"  ==  "P" set port=8080
 copy /Y %courant%\Git\etc\gitconfig %courant%\Git\etc\gitconfig.save
 echo [https] >> %courant%\Git\etc\gitconfig
 echo     proxy = %login%:%password%@%proxy%:%port% >> %courant%\Git\etc\gitconfig
-REM %courant%\Git\bin\git.exe reset --hard HEAD
-%courant%\Git\bin\git.exe clean -d -f
+%courant%\Git\bin\git.exe clean -d -f -x
 %courant%\Git\bin\git.exe remote add -t distribution-dev origin https://github.com/fabnicol/altair.git
 %courant%\Git\bin\git.exe fetch origin distribution-dev
 %courant%\Git\bin\git.exe merge -s recursive -X theirs FETCH_HEAD
