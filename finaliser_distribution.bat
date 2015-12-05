36370a9f985067042a01ebfc7038009047b2387a
Fabrice Nicol <fabrnicol@gmail.com>
2015-12-05 12:21:08 +0100

    restor Rproj and system dirs

--- finaliser_distribution.bat
+++ finaliser_distribution.bat
@@ -3,25 +3,31 @@ echo Actualisation de la copie locale de l'entrepot
 set courant=%~dp0\Altair
 cd %courant%
 %courant%\Git\bin\git.exe --version
+set /p login="Entrez votre login de session : "
+set /p password="Entrez votre mot de passe de session : "
+set /p proxy="Entrez le proxy [P pour 10.163.251.190]: "
+set /p port="Entrez le port [P pour 8080]: "
+if "%proxy%" ==  "P" set proxy=10.163.251.190
+if "%port%"  ==  "P" set port=8080
+%courant%\Git\bin\git.exe config --global http.proxy http://%login%:%password%@%proxy%:%port% 
+%courant%\Git\bin\git.exe config --global https.proxy https://%login%:%password%@%proxy%:%port% 
 %courant%\Git\bin\git.exe config --global credential.helper.cache  wincred 
 %courant%\Git\bin\git.exe config --global credential.helper  wincred 
 %courant%\Git\bin\git.exe config --global user.name  "Fabrice Nicol"
 %courant%\Git\bin\git.exe config --global user.email   fabrnicol@gmail.com 
 %courant%\Git\bin\git.exe init
 %courant%\Git\bin\git.exe add -f Git
-%courant%\Git\bin\git.exe commit -am "Start" --quiet
+%courant%\Git\bin\git.exe commit -am "Start"
 %courant%\Git\bin\git.exe clean -d -f -x 
 %courant%\Git\bin\git.exe remote add -t distribution origin https://github.com/fabnicol/altair.git
 %courant%\Git\bin\git.exe fetch --depth=1 origin distribution
-%courant%\Git\bin\git.exe merge --quiet -s recursive -X theirs FETCH_HEAD
+%courant%\Git\bin\git.exe merge -s recursive -X theirs FETCH_HEAD
 xcopy /I /Y /E %courant%\Roaming\RStudio  %USERPROFILE%\AppData\Roaming\RStudio
 xcopy /I /Y /E %courant%\Local\RStudio-desktop  %USERPROFILE%\AppData\Local\RStudio-desktop
 %courant%\Git\bin\git.exe rm -rf  --cached Tests\Exemple\Donnees\R-Al*
 %courant%\Git\bin\git.exe rm -rf --cached .Rproj.user
 %courant%\Git\bin\git.exe rm -f  --cached lhx\cl
-mkdir %courant%\Tests\Exemple\Donnees\R-Altaïr
-mkdir %courant%\lhx
-%courant%\Git\bin\git.exe commit --quiet -a  -m "local mods"
+%courant%\Git\bin\git.exe commit -a  -m "local mods"
 %courant%\Git\bin\git.exe config --global http.proxy 
 %courant%\Git\bin\git.exe config --global https.proxy 
 REM END OF FILE
