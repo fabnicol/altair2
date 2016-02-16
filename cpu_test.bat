9253c3efde31dff2d9d913bb49150f1898f6bfec
Fabrice Nicol <fabrnicol@gmail.com>
2016-02-16 19:36:45 +0100

    celeron mod

--- cpu_test.bat
+++ cpu_test.bat
@@ -1,4 +1,5 @@
 @echo off
-for /f "tokens=2 delims==" %%A in ('wmic cpu get name /value') do @(echo "%%A") | find /I "core2" >nul && (echo 1 > cpu_type) || (echo 0 > cpu_type)
+for /f "tokens=2 delims==" %%A in ('wmic cpu get name /value') do @(echo "%%A") | ((echo "%%A" | find /I  "core2") || (echo "%%A" | find /I "celeron"))  >nul && (echo 1 > cpu_type) || (echo 0 > cpu_type)
+
 REM puis lire cpu_type
 
