8758f61a086f81393944b8b339b677a22c8c9220
Fabrice Nicol <fabrnicol@gmail.com>
2016-02-24 02:43:02 +0100

    new rmarkdown

--- Tests/Exemple/sft.R
+++ Tests/Exemple/sft.R
@@ -8,7 +8,7 @@
 
 # part fixe mensuelle
 
-sft.fixe <- c(un = 2.29, deux = 10.67, trois = 15.24, 15.24 + 4.57 * 1:12)
+r2c::r2c(sft.fixe <- c(un = 2.29, deux = 10.67, trois = 15.24, 15.24 + 4.57 * 1:12))
 
 # part variable en proportion du traitement indiciaire
 
