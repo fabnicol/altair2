57d322ad19874f09dad7c05597f783c77b9576c8
Nicol, Fabrice <fabrice.nicol@crtc.ccomptes.fr>
2016-08-03 19:38:44 +0200

    amélioration détection corps FPH (boschet-lemen-degoul)

--- lhx/validator.hpp
+++ lhx/validator.hpp
@@ -35,16 +35,23 @@ static constexpr auto EXPRESSION_REG_ELUS = "^maire.*|^pr..?sident.*|^[eé]lus?|
   EXPRESSION_REG_VACATIONS = ".*\\bvacat.*|.*\\bvac\\.?\\b.*",                 // vac.* peut être vérifié par 'vacances'
   EXPRESSION_REG_ASSISTANTES_MATERNELLES = ".*\\bass.*\\bmater.*",
   EXPRESSION_REG_ADJOINTS = "\\W*(?:adj.*(?:adm|ani|tech|pat)|op[eé].*(a\\.?p\\.?s\\.?|act)|aux.*(pu[eé]r|soin)|gard(ien|.*ch)|brigadier|receveur|sapeur|capor|sous.*off).*",
-  EXPRESSION_REG_AGENTS = "\\W*(?:(?:agent|agt\\.?).*(?:soc|ma[îi]|poli|p\\.?m\\.?|sp[eé]|pat)|(?:agent|agt\\.?)?.*atsem).*",
+  EXPRESSION_REG_AGENTS = "\\W*(?:(?:agent|agt\\.?).*(?:ser.*ho|soc|ma[îi]|poli|p\\.?m\\.?|sp[eé]|pat)|\
+(?:agent|agt\\.?)?.*atsem|aide.*(?:soi|pha)|aumonier|cond.*amb|dessin|ouvrier).*",
 
  /* Attention il ne faut pas autre chose que \\W* car sinon on peut avoir confusion entre cons[eiller].* et [agent].*cons[ervation].*   */
  /* cons = conseiller ou conservateur souvent abrégé cons., mai speut être aussi conservation */
 
-  EXPRESSION_REG_CAT_A = "\\W*(?:adminis|attach|biol|biblio|infi.*(?:cad.*san|soi)|cap.*t|com.*t|.*colon|cons\\.?|d\\S*\\.?\\s*g\\S*\\.?|direct|ing[eé]n|m[eé]de|pharm|prof|psy|puer|puér|sage.*f|secr.*mai[v[eé]t[eé]r]|infirm\\S*\\s.*(?:soi|enc.*s|C\\.?S\\.?)).*",
+  EXPRESSION_REG_CAT_A = "\\W*\
+(?:adminis|a.*\\bh.*\\bu|c.*\\b(?:cl|tr).*\\bu|attach|biol|biblio|cad.*soc.*ed|cap.*t|com.*t|.*colon|cons\\.?|d\\S*\\.?\\s*g\\S*\\.?|\
+dir(?:ect|.*\\b[eé]t.*b|.*\\bsoi)|ing[eé]n|m[eé]de|ma.t.*conf|prat.*hos|pharm|p[eé]d.*p.*c.*\\bs|pr[eé]p.*c.*\\bs|prof|psy.*(?:l|m.*c.*\\bs)|puer|puér|sage.*f|secr.*mai[v[eé]t[eé]r]|\
+i\\.?a\\.?d\\.?e\\.?|i\\.?b\\.?o\\.?d\\.?e\\.?|infi?r?m?i?[eè]?r?\\.?\\s*(?:anes|bloc|i\\.?a\\.?d\\.?|i\\.?b\\.?o\\.?d\\.?|s\\.?\\s*\\bg\\.?|soi|enc.*s)|\
+ergot|radiophys|(?:tec.*l|mass.*kin|di[eé]t|inf|manip).*\\bc(?:\\.|a).*\\bs).*",
 
- /* A cause du cas problématique des infirmiers, ex B recatégorisés en A, il faut d'abord tester A puis si échec B */
+ /* A cause du cas problématique des infirmiers et diététiciens, ex B recatégorisés en A, il faut d'abord tester A puis si échec B */
 
-  EXPRESSION_REG_CAT_B = "\\W*(?:r[eé]dac|tech|anim|[eé]duc|ass\\.?.*(?:ens|cons|pat|bib|socio.*[eé]d|m[ée]d.*t)|monit|contr[oô].*t(?:er|ra)|chef.*p.*m|lieut[^c\\s]*\\b|infirm|r[eé][eé]duc).*",
+  EXPRESSION_REG_CAT_B = "\\W*\
+(?:r[eé]dac|tech|anim|[eé]duc|a\\.?\\s?s\\.?\\s?(?:e|\\s)|assi?s?t?\\.?.*(?:sp[eé]c|ens|cons|pat|bib|soc.*[eé]du?c?|m[ée]d.*t)|monit|contr[oô].*t(?:er|ra)|\
+chef.*p.*m|lieut[^c\\s]*\\b|infirm|r[eé][eé]duc|adj.*cadr|analyst|di[eé]t|pr[eé]par.*ph|p[eé]d.*po|programmeu|orthop|mass.*kin|manip).*",
 
 /* Les définitions ci-après doivent être négatives */  NOM_BASE = "Table",
   NOM_BASE_BULLETINS = "Bulletins",
