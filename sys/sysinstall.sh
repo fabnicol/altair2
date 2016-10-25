a2140e5e3295c356e580b12a4443e0719d84862b
Fabrice Nicol <fabrice.nicol@crtc.ccomptes.fr>
2016-10-25 15:46:48 +0200

    Amélioration de la création des comptes utilisateurs

--- sys/sysinstall.sh
+++ sys/sysinstall.sh
@@ -1,2 +1,4 @@
 cp -vf sys/10-agent-shutdown.sh /etc/plasma/shutdown
 cp -vf sys/m.sh /etc/profile.d
+cp -vf compte_utilisateur.sh /usr/bin
+sed -i 's/ALL ALL=(ALL) ALL/#ALL ALL=(ALL) ALL/' /etc/sudoers
