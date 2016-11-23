d1ca7fd9d9b5738091d74fbc961e8df44ef5182d
Nicol, Fabrice <fabrice.nicol@crtc.ccomptes.fr>
2016-11-23 18:35:34 +0100

    Préparation à la distribution du noyau

--- sys/sysinstall.sh
+++ sys/sysinstall.sh
@@ -2,3 +2,8 @@ cp -vf sys/10-agent-shutdown.sh /etc/plasma/shutdown
 cp -vf sys/m.sh /etc/profile.d
 cp -vf compte_utilisateur.sh /usr/bin
 sed -i 's/ALL ALL=(ALL) ALL/#ALL ALL=(ALL) ALL/' /etc/sudoers
+if test -d sys/kernel; then
+  cp -vf sys/kernel/*   /boot
+  grub-mkconfig -o /boot/grub/grub.cfg
+  echo "Installation du noyau réalisée."
+fi
