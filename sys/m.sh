7557f418fa068cf28131a0f914646c1eca4ac337
Fabrice Nicol <fabrice.nicol@crtc.ccomptes.fr>
2016-11-27 06:08:56 +0100

    Améliorations des fichiers système

--- sys/m.sh
+++ sys/m.sh
@@ -1,16 +1,16 @@
 
 # La clé est reconnue par sa dimension de 64 Go, par son formatage exfat et son caractère de disque USB
 
-cle=/dev/$(lsblk -O | grep ".*exfat.*58,4G.*part.*scsi:usb" | cut -f 2 -d \ )
+cle=/dev/$(lsblk -O | grep -E ".*exfat.*[0-9,.]{2,5}G.*part.*scsi:usb" | cut -f 3 -d \ )
 
 DATA=$HOME/Dev/altair/Tests/Exemple/Donnees
 
-udisksctl mount --no-user-interaction -b $cle
+udisksctl mount --no-user-interaction --options nonempty -b $cle
 
 if test -h $DATA/R-Altair; then rm  $DATA/R-Altair; fi	
 TARGET=$(find /run/media/$USER -mindepth 1 -maxdepth 1 -type d)
 echo "Found TARGET=$TARGET"
-if test -d $TARGET; then
+if test -d "$TARGET"; then
     echo "Linking $DATA/R-Altair to $TARGET"
     cd $DATA
     ln -s  -v  $TARGET R-Altair
