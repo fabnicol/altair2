640dba63d3cfb366712d0432dc59df5b11c8a097
Fabrice Nicol <fabrice.nicol@crtc.ccomptes.fr>
2016-12-07 13:54:31 +0100

    ajout du service ajuster_m

--- sys/10-agent-shutdown.sh
+++ sys/10-agent-shutdown.sh
@@ -18,16 +18,36 @@
 
 # Tester /dev/sdb1, /dev/sdc1, /dev/sdd1
 
-# La clé est reconnue par sa dimension de 64 Go, par son formatage exfat et son caractère de disque USB
+# La clé est reconnue par sa dimension de XYZTU Go, par son formatage exfat et son caractère de disque USB
 
 cle=/dev/$(lsblk -O | grep -E ".*exfat.*[0-9,.]{2,5}G.*part.*scsi:usb" | cut -f 3 -d \ )
 
 
 udisksctl unmount --force --no-user-interaction --block-device  $cle
+
+# Il faut effacer le lien symbolique pour éviter des problème de conflits potentiels de clés si les clés changent entre deux connexions
+# Il sera recréé à la prochaine connection
+
 if test -h ~/Dev/altair/Tests/Exemple/Donnees/R-Altair; then
   rm ~/Dev/altair/Tests/Exemple/Donnees/R-Altair
+else
+
+# Il faut effacer les donssiers "durs" qui pourraient avoir été créés en l'absence de clé. En effet la création d'un lien symbolique pourrait échouer "par dessus"
+
+if test -d ~/Dev/altair/Tests/Exemple/Donnees/R-Altair; then
+  rm -rf ~/Dev/altair/Tests/Exemple/Donnees/R-Altair
 fi
+
+
+# Probablement inutile mais des cas de problèmes sont rapportés dans les forums
 rm ~/.Xauthority
+
+
+# .Rproj.user doit être raffraichi à chaque connection pour actualiser les droits d'accès. Sinon RStudio démarerra sans les panneaux 
+# de scripts ajustés par défaut
+# L'expérience montre que ceci est difficulement réalisable par chown/chmod L'utilisation d'une recopie depui /home/Public à la connexions
+# est empiriquement testés comme à la fois plus simple et plus sûre. 
+
 
 rm -rf $HOME/Dev/altair/.Rproj.user
 
