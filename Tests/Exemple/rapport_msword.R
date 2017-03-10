53970abb394fc9416e475e737a60ffea8d11d1ac
Fabrice Nicol <fabrnicol@gmail.com>
2017-03-10 20:18:48 +0100

    Adjonction de la licence, des en-têtes de licence

--- Tests/Exemple/rapport_msword.R
+++ Tests/Exemple/rapport_msword.R
@@ -1,9 +1,46 @@
+# Copyright Cour des comptes, 2017
+# Contributeur :
+# Fabrice Nicol, années 2012 à 2017
+# fabrice.nicol@crtc.ccomptes.fr
+# 
+# Ce logiciel est un programme informatique servant à extraire et analyser les fichiers de paye
+# produits au format spécifié par l'annexe de la convention-cadre nationale de dématérialisation
+# en vigueur à compter de l'année 2008.
+# 
+# Ce logiciel est régi par la licence CeCILL soumise au droit français et
+# respectant les principes de diffusion des logiciels libres. Vous pouvez
+# utiliser, modifier et/ou redistribuer ce programme sous les conditions
+# de la licence CeCILL telle que diffusée par le CEA, le CNRS et l'INRIA
+# sur le site "http://www.cecill.info".
+# 
+# En contrepartie de l'accessibilité au code source et des droits de copie,
+# de modification et de redistribution accordés par cette licence, il n'est
+# offert aux utilisateurs qu'une garantie limitée. Pour les mêmes raisons,
+# seule une responsabilité restreinte pèse sur l'auteur du programme, le
+# titulaire des droits patrimoniaux et les concédants successifs.
+# 
+# A cet égard l'attention de l'utilisateur est attirée sur les risques
+# associés au chargement, à l'utilisation, à la modification et/ou au
+# développement et à la reproduction du logiciel par l'utilisateur étant
+# donné sa spécificité de logiciel libre, qui peut le rendre complexe à
+# manipuler et qui le réserve donc à des développeurs et des professionnels
+# avertis possédant des connaissances informatiques approfondies. Les
+# utilisateurs sont donc invités à charger et tester l'adéquation du
+# logiciel à leurs besoins dans des conditions permettant d'assurer la
+# sécurité de leurs systèmes et ou de leurs données et, plus généralement,
+# à l'utiliser et l'exploiter dans les mêmes conditions de sécurité.
+# 
+# Le fait que vous puissiez accéder à cet en-tête signifie que vous avez
+# pris connaissance de la licence CeCILL, et que vous en avez accepté les
+# termes.
+# 
+# 
 source("prologue_rapport.R", encoding = "ISO-8859-1")
 source ("rendre.R", encoding = encodage.code.source)
 
 envir <- rendre()
 
-file.rename("altair.docx", "altaÃ¯r.docx")
+file.rename("altair.docx", "altaïr.docx")
 
 ajuster_chemins_odt(hack_md())
 
@@ -13,7 +50,7 @@ system(
    "altair.md +RTS -K512m -RTS --to",
    "odt",
    "--from markdown+autolink_bare_uris+ascii_identifiers+tex_math_single_backslash-implicit_figures --highlight-style tango --output",
-   "altaÃ¯r.odt")
+   "altaïr.odt")
 )
 
 if (! keep_md) {
@@ -24,25 +61,25 @@ if (! keep_md) {
 
 if (setOSWindows) {
   
-  system("xcopy /Y altaÃ¯r.docx Donnees\\R-AltaÃ¯r")
-  system("xcopy /Y altaÃ¯r.odt  Donnees\\R-AltaÃ¯r")
-  system("mkdir Donnees\\R-AltaÃ¯r\\Docs")
-  system("xcopy /E /Y Docs Donnees\\R-AltaÃ¯r\\Docs")
+  system("xcopy /Y altaïr.docx Donnees\\R-Altaïr")
+  system("xcopy /Y altaïr.odt  Donnees\\R-Altaïr")
+  system("mkdir Donnees\\R-Altaïr\\Docs")
+  system("xcopy /E /Y Docs Donnees\\R-Altaïr\\Docs")
   
 } else {
   
   system("cp -rf Docs Donnees/R-Altair")
-  system("cp -f altaÃ¯r.odt altaÃ¯r.docx Donnees/R-Altair")
+  system("cp -f altaïr.odt altaïr.docx Donnees/R-Altair")
 }
 
 
 if (setOSWindows) {
   
-  shell("start winword Donnees/R-AltaÃ¯r/altaÃ¯r.docx")
+  shell("start winword Donnees/R-Altaïr/altaïr.docx")
   
 } else {
   
-  system("lowriter Donnees/R-Altair/altaÃ¯r.odt")
+  system("lowriter Donnees/R-Altair/altaïr.odt")
   
 }
 
