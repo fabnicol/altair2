dcad3670c1feeb6ca1532364dd46a5630ae2b5b6
Fabrice Nicol <fabrice.nicol@crtc.ccomptes.fr>
2017-03-27 18:01:16 +0200

    amélioration des droits sur fab

--- Actualiser.sh
+++ Actualiser.sh
@@ -7,89 +7,92 @@ echo "Oui/Non : "
 
 read reponse
 
-if test x$reponse = xOui -o x$reponse = xoui
+if test x$reponse ! xOui -a x$reponse != xoui
+then 
+  echo "*** L'actualisation a été annulée ***"
+  exit 0 
+fi  
+
+echo "****"  
+echo "* Actualisation du dépôt fab..."
+echo "****"
+cd /home/fab/Dev/altair
+git reset --hard HEAD
+git clean -df
+adresse=$(cat entrepot.txt)
+
+current_origin=$(git remote -v | grep origin | grep fetch | cut -f'2' | cut -f 1 -d' ')
+
+git config user.name "Fabrice Nicol"
+git config user.email "fabrice.nicol@crtc.ccomptes.fr"
+git config --unset http.proxy 
+git config --unset https.proxy
+git config http.sslVerify false
+git config https.sslVerify false
+git config credential.helper store
+
+if ! test x$current_origin = x$adresse
 then
-  echo "****"  
-  echo "* Actualisation du dépôt fab..."
-  echo "****"
-  cd /home/fab/Dev/altair
-  git reset --hard HEAD
-  git clean -df
-  adresse=$(cat entrepot.txt)
-  
-  current_origin=$(git remote -v | grep origin | grep fetch | cut -f'2' | cut -f 1 -d' ')
-
-   git config user.name "Fabrice Nicol"
-   git config user.email "fabrice.nicol@crtc.ccomptes.fr"
-   git config --unset http.proxy 
-   git config --unset https.proxy
-   git config http.sslVerify false
-   git config https.sslVerify false
-   git config credential.helper store
-
-  if ! test x$current_origin = x$adresse
-  then
-    echo "****"
-    echo "* Actualisation du dépôt $adresse (ancien $current_origin). Patientez..."
-    echo "***"
-    git remote remove origin
-    git remote add -t master origin $adresse
-  fi
-  
-  git fetch -p -n --depth=1 origin master-jf
-  
-  for i in altair.linux sft Docs Interface lhx Interface_linux linux '*.txt' '*.R' '*.sh' '*.desktop' VERSION LICENCE '*.ico' '*.bmp' '*.png'  postinstall.sh altaïr.Rproj 'Tests/Exemple' 'Tests/Exemple/Docs' sys
-  do
-    git checkout FETCH_HEAD -- "$i" 
-    git add .
-  done
-  
-  if ! test -d  lhx
-  then
-     mkdir lhx    
-  fi
-
-  if test -f sys/actualiser_sys -a -f sys/sysinstall.sh; then
-    sudo --preserve-env /bin/bash sys/sysinstall.sh
-    if test $? = 0; then
-       echo "***"
-       echo "* Actualisation système effectuée"
-       echo "***"
-       sleep 3
-       git rm -f sys/actualiser_sys
-    else
-       echo "=> Erreur d'actualisation système"
-    fi
-  fi
-  
-  git commit -am "Sauvegarde $(date)"
-  
-  echo "****"
-  echo "* Actualisation de jf..."
-  echo "****"
-      
-  for i in altair.linux sft Docs Interface_linux linux '*.txt'  '*.R'  '*.sh'  '*.desktop' VERSION LICENCE '*.ico'  '*.bmp'  '*.png'  postinstall.sh altaïr.Rproj Tests
-  do
-    sudo cp -rvf "/home/fab/Dev/altair/$i" /home/jf/Dev/altair
-    sudo chown -R jf "/home/jf/Dev/altair/$i"
-  done
-  
-  sudo chmod 0770 /home/jf/Dev/altair/Tests/Exemple/*.*
-  sudo chmod 0770 -R /home/jf/Dev/altair/Interface_linux/gui/x64
-  sudo chmod 0770 /home/jf/Dev/altair/*.*
-  sudo chgrp -R users /home/jf/Dev/altair
-  rm -f /home/jf/Dev/altair/.directory
-  
-  if ! test -d  /home/jf/Dev/altair/lhx
-  then
-     mkdir /home/jf/Dev/altair/lhx    
-     sudo chown jf    /home/jf/Dev/altair/lhx
-     sudo chmod -R 0777  /home/jf/Dev/altair/lhx
-  fi
-  
-  cd /home/fab/Dev/altair
-  git gc --prune=now
+echo "****"
+echo "* Actualisation du dépôt $adresse (ancien $current_origin). Patientez..."
+echo "***"
+git remote remove origin
+git remote add -t master origin $adresse
 fi
+
+git fetch -p -n --depth=1 origin master-jf
+
+for i in altair.linux sft Docs Interface lhx Interface_linux linux '*.txt' '*.R' '*.sh' '*.desktop' VERSION LICENCE '*.ico' '*.bmp' '*.png'  postinstall.sh altaïr.Rproj 'Tests/Exemple' 'Tests/Exemple/Docs' sys
+do
+git checkout FETCH_HEAD -- "$i" 
+git add .
+done
+
+if ! test -d  lhx
+then
+ mkdir lhx    
+fi
+
+if test -f sys/actualiser_sys -a -f sys/sysinstall.sh; then
+sudo --preserve-env /bin/bash sys/sysinstall.sh
+if test $? = 0; then
+   echo "***"
+   echo "* Actualisation système effectuée"
+   echo "***"
+   sleep 3
+   git rm -f sys/actualiser_sys
+else
+   echo "=> Erreur d'actualisation système"
+fi
+fi
+
+git commit -am "Sauvegarde $(date)"
+
+echo "****"
+echo "* Actualisation de jf..."
+echo "****"
+  
+for i in altair.linux sft Docs Interface_linux linux '*.txt'  '*.R'  '*.sh'  '*.desktop' VERSION LICENCE '*.ico'  '*.bmp'  '*.png'  postinstall.sh altaïr.Rproj Tests
+do
+sudo cp -rvf "/home/fab/Dev/altair/$i" /home/jf/Dev/altair
+sudo chown -R jf "/home/jf/Dev/altair/$i"
+done
+
+sudo chmod 0770 /home/jf/Dev/altair/Tests/Exemple/*.*
+sudo chmod 0770 -R /home/jf/Dev/altair/Interface_linux/gui/x64
+sudo chmod 0770 /home/jf/Dev/altair/*.*
+sudo chgrp -R users /home/jf/Dev/altair
+rm -f /home/jf/Dev/altair/.directory
+
+if ! test -d  /home/jf/Dev/altair/lhx
+then
+ mkdir /home/jf/Dev/altair/lhx    
+ sudo chown jf    /home/jf/Dev/altair/lhx
+ sudo chmod -R 0777  /home/jf/Dev/altair/lhx
+fi
+
+cd /home/fab/Dev/altair
+git gc --prune=now
 
 echo "Actualisation des documents de référence sur le bureau"
 
@@ -112,6 +115,8 @@ do
 	 sudo chgrp -R users /home/$i/.local/share/applications
    fi
 done
+
+sudo chown -R fab .
 
 echo "****" 
 echo "* Fin de l'opération."
