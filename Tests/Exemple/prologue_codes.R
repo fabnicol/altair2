75e3c9cb35fa99e67556884602b8dd4362cf79bd
Nicol, Fabrice <fabrice.nicol@crtc.ccomptes.fr>
2017-07-06 16:19:41 +0200

    améliorations sur rappels NBI

--- Tests/Exemple/prologue_codes.R
+++ Tests/Exemple/prologue_codes.R
@@ -53,7 +53,7 @@ codes.ps  <- NA
 codes.ptech <- NA  
 types.irs  <- c("IR", "S")
 
-controle<-c("","","","")  
+controle<-c("Z","Z","","")  
 
 codes <- data.table(type = c("TRAITEMENT", "NBI", "PFI", "PFR", "PSR", "IPF", "IFTS", "IAT", "IHTS", "VAC", "ASTREINTES", "PRIME SPECIFIQUE", "IFT", "PRIME DE SERVICE", "PRIME DE TECHNICITE", "IR_S"),
                     valeur = list(
