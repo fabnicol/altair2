d86afebac1eb4d9561a6a5d052a0c417f3d4ff96
Fabrice Nicol <fabrice.nicol@crtc.ccomptes.fr>
2017-08-25 06:42:04 +0200

    Corrections sur flux.R

--- Tests/Exemple/flux.R
+++ Tests/Exemple/flux.R
@@ -1,14 +1,21 @@
 library(readr)
+library(data.table)
+
 CRC_BASE3 <- read_delim("~/Desktop/SDIS/Bases/CRC-BASE3.csv",
 ";", escape_double = FALSE, locale = locale(date_names = "fr",
 grouping_mark = " ", encoding = "WINDOWS-1252"),
 trim_ws = TRUE)
+CRC_BASE3$Matricule <- as.character(CRC_BASE3$Matricule)
+CRC_BASE3$Mois <- as.integer(CRC_BASE3$Mois)
+
+Bulletins.paie[Grade %chin% grades.pats, Type.personnel := "PATS"]
+Bulletins.paie[Grade %chin% grades.spp, Type.personnel  :=  "SPP"]
 
 # les exécutions précédentes doivent avoir produit Bulletins.paie[Matricule, Année, Mois, Type.personnel = "SPP" ou "PATS"] à fusionner avec CRC_BASE3
 # veiller à ce que la fusion soit propre :"SPP", "PATS" ou "NA"
 
 CRC_BASE3 <- as.data.table(CRC_BASE3)
-merge(CRC_BASE3, Bulletins.paie, by = c("Matricule", "Année", "Mois", "Type.personnel"))
+CRC_BASE3 <- merge(CRC_BASE3, Bulletins.paie[, .(Matricule, Année, Mois, Type.personnel)], by = c("Matricule", "Année", "Mois"), all = TRUE)
 
 # write.csv2(unique(CRC_BASE3$Evenement), "CRC_BASE3_E.csv")
 # Catégoriser et classer dans Type_sortie et Type_entrée
@@ -18,39 +25,45 @@ CRC_BASE3_E <- read_delim("CRC_BASE3_E.csv",
 
 # Sorties
 
-CRC_BASE_3_1 <- merge(CRC_BASE3, CRC_BASE3_E[, -1])
+CRC_BASE_3_1 <- merge(CRC_BASE3, CRC_BASE3_E[, -1], by = "Evenement", all = TRUE)
 Base_3_2 <- CRC_BASE_3_1[! is.na(Type_sortie)]
 Base_3_3 <- CRC_BASE_3_1[! is.na(Type_entrée)]
 
-flux <- function (type) {
+flux <- function (type = "Total") {
   
-  Sorties <- Base_3_2[! is.na(Type.personnel) & Type.personnel == type , .(Cum = uniqueN(Matricule)), by = .(Type_sortie, Année)]
+  if (type == "Total") {
+    Sorties <- Base_3_2[ , .(Cum = uniqueN(Matricule)), by = .(Type_sortie, Année)]
+  } else {
+    Sorties <- Base_3_2[! is.na(Type.personnel) & Type.personnel == type , .(Cum = uniqueN(Matricule)), by = .(Type_sortie, Année)]  
+  }
+  
   
   S <- dcast(Sorties, Type_sortie ~ Année, value.var = "Cum")
   
   # Remplacer les NA par des 0
   
-  for (x in names(T)) T[is.na(get(x)), (x) := 0]
+  for (x in names(S)) S[is.na(get(x)), (x) := 0]
   
   # Marge colonnes en cumuls
   
   tot <- c("Total", colSums(S[, -1], na.rm = TRUE))
   names(tot)[[1]] <- "Type_sortie"
   T <- rbind(S, t(tot))
-  T[[1]] <- c("Autre", "Décès", "Disponibilité-Congé parental", "Démission", "Détachement", "Inaptitude", "Licenciement", "Mutation", "Retraite", "Total")
+  
   names(T)[[1]] <- ( "Année")
   
   # Marge lignes en variations 
-  
+  n <- ncol(T) 
   T[ , Variation := 0]
-  n <- ncol(T)
-  for (i in 1:nrow(T)) set(T, i, n, round((T[i, n]/T[i, 2] - 1) * 100, digits = 1))
   
-  write.csv2(T, "Flux.sortie." %+% catégorie.personnel +".csv")
+  for (i in 2:nrow(T)) set(T, i, n + 1, ifelse(T[i, 2] != 0 & !is.na(T[i, n, with = FALSE]),
+                                           round((as.numeric(T[i, n, with = FALSE])/(as.numeric(T[i, 2]) - 1)) * 100, digits = 1), NA))
+  
+  write.csv2(T, "Flux.sortie." %+% type %+% ".csv")
 
   T
 }
 
-
-Sorties.SPP <- flux("SPP")
+Sorties.SPP  <- flux("SPP")
 Sorties.PATS <- flux("PATS")
+Sorties.total <- flux("Total")
