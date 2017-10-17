80ea70a0239b83e2360ca09a9d00ae17facde832
Nicol <fabrice.nicol@crtc.ccomptes.fr>
2017-10-17 20:48:21 +0200

    ajout d'un nettoyage de biblio. altair sous /usr/lib

--- sys/Rlibrary/altair/R/effectifs.R
+++ sys/Rlibrary/altair/R/effectifs.R
@@ -1,4 +1,43 @@
 
+# Auteur : Fabrice Nicol
+# Copyright Fabrice Nicol
+# Contributeur :
+# Fabrice Nicol, années 2008 à 2012, 2017
+# fabrnicol@gmail.com
+#
+# Ce logiciel est régi par les dispositions du code de la propriété
+# intellectuelle. 
+
+# L'auteur se réserve le droit d'exploitation du présent logiciel, 
+# et notamment de reproduire et de modifier le logiciel, conformément aux 
+# dispositions de l'article L.122-6 du même code. L'auteur se réserve le droit
+# de modifier et de corriger les erreurs du logiciel, conformément aux termes 
+# du deuxième alinéa du I de l'article L.122-6-1 du même code.
+
+# Le présent logiciel doit être regardé comme un "Module Externe", pour 
+# l'application des stipulations de la licence CeCILL, lorsque le code source, 
+# ou une bibliothèque compilée à partir du présent code, sont utilisés pour 
+# compiler tout logiciel faisant usage du présent code.
+
+# Tout fichier contenant le présent code, ou des versions antérieures du 
+# présent code, et faisant apparaître des termes contraires aux stipulations
+# précédentes doit être regardé comme faisant partie d'actes préparatoires à
+# l'élaboration du présent logiciel et non comme le logiciel lui-même, ou 
+# comme des erreurs matérielles dans la copie des licences. Seules les 
+# stipulations précédentes doivent être regardées comme s'appliquant au 
+# présent logiciel.
+#
+# Le fait que vous puissiez accéder à cet en-tête signifie que vous avez
+# pris connaissance de ces stipulations et que vous en avez accepté les
+# termes.
+
+# Pour l'année 2017, une autorisation d'usage, de modification et de 
+# reproduction du présent code est donnée à tout agent employé par les
+# juridictions financières. Cette autorisation est temporaire et peut être 
+# révoquée.
+#
+
+
 
 
 # personnels
