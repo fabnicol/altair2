8a1dc496f7dbeb01e0add36a49a316574f22eee8
Fabrice Nicol <fabrice.nicol@crtc.ccomptes.fr>
2018-01-01 22:59:44 +0100

    doc

--- Docs/html/classprocess_page.js
+++ Docs/html/classprocess_page.js
@@ -7,7 +7,7 @@ var classprocess_page =
     [ "logCheckBox", "classprocess_page.html#afff5e1e538d2323b3f51be1b3aa8ee6a", null ],
     [ "logFrame", "classprocess_page.html#abcd65c24aa393862df1157744c5d5893", null ],
     [ "memoryUseWidget", "classprocess_page.html#ab7f8bfa8a20f66a31b45453567661e82", null ],
-    [ "nLineLabel", "classprocess_page.html#a7fd9ecff2b8ce665c5c7754e03d48733", null ],
+    [ "nLineLabel", "classprocess_page.html#abf516891bb2e8b5e24f29f4154414277", null ],
     [ "NLineLabel", "classprocess_page.html#a1064f9e89ccd7b8a9832a7a63258898b", null ],
     [ "processTypeBox", "classprocess_page.html#a1c7d28b202a68378863df4dbb893049f", null ],
     [ "processTypeWidget", "classprocess_page.html#a09a794350e10b6053ed4f2abe7515f64", null ],
