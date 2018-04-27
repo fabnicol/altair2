eb4e8f7600ffcf2bc417582779386088f43f2c50
Fabrice Nicol <fabrnicol@gmail.com>
2018-04-27 03:46:27 +0200

    Quasi finalisation nouveau framework primes

--- Tests/Exemple/test.R
+++ Tests/Exemple/test.R
@@ -6,8 +6,17 @@
 # si non null Paie_B doit avoir indic_B
 
 
-test <- function(prime, Paie_I, Paie_B = NULL, verbeux) {
+test <- function(prime, Paie_I, Paie_B = NULL, Lignes_B = NULL, verbeux = FALSE) {
 
+# Initialisation
+  
+Paie_A <- NULL 
+Lignes_A <- NULL 
+personnels.A.B <- NULL 
+A.non.cat <- NULL 
+nombre.mois.cumuls <- 0 
+nombre.agents.cumulant.A.B <- 0
+  
 ident_prime <- prime$nom
 
 # prime A testée contre prime B
@@ -15,9 +24,19 @@ ident_prime <- prime$nom
 résultat.manquant <- FALSE
 nombre.agents.cumulant.A.B <- 0
 
-Paie_A   <- filtrer_Paie(ident_prime, portée = "Mois", Base = Paie_I, indic = TRUE)
-Lignes_A <- Paie_A[indic == TRUE][ , indic := NULL]
+essayer({
+          Paie_A   <- filtrer_Paie(ident_prime, portée = "Mois", Base = Paie_I, indic = TRUE)
+          Lignes_A <- Paie_A[indic == TRUE
+                             ][ , indic := NULL]
+          
+          
+}, 
+"Le filtrage des idemnités " %+% ident_prime %+% " n'a pas pu être réalisé.")
 
+# Questions de compatibilité statutaire
+
+essayer({
+  
 if (prime$restreint_fonctionnaire) {
    
    A.non.tit  <- Lignes_A[Statut != "TITULAIRE" & Statut != "STAGIAIRE"]
@@ -47,22 +66,28 @@ if (prime$catégorie != ""){
     cat("Tous les attributaires de", ident_prime, "sont identifiés en catégorie", prime$catégorie)
   }
 }
+}, "La détection des incompatibilités statutaires n'a pas pu être réalisée.")
 
+
+essayer({
+  
+  
 K <- "codes." %+% tolower(ident_prime)
 
 if (exists(K) && is.na(get(K))) {
   
   assign(K, list(K = unique(Lignes_A$Code)))
-
+  
   L <- length(get(K))
   
   if (L == 0) {
-    cat("Il n'a pas été possible d'identifier", ident_prime, "par méthode heuristique. Renseigner les codes de paye correspondants dans l'interface graphique.")
+    cat("Il n'a pas été possible d'identifier", 
+        ident_prime,
+        "par méthode heuristique. Renseigner les codes de paye correspondants dans l'interface graphique.")
     résultat.manquant <- TRUE
   }
 }
-
-
+  
 if (! is.null(Paie_B) && ! résultat.manquant) {
   
   # on exclut les rappels !
@@ -84,17 +109,23 @@ if (! is.null(Paie_B) && ! résultat.manquant) {
   
   personnels.A.B <- B_[indic == TRUE | get(indic_B) == TRUE
                       ][ , indic := NULL
-                      ][ , indic_B := NULL, with = FALSE]
+                      ][ , indic_B := NULL
+                           , with = FALSE]
   
   nombre.mois.cumuls <- uniqueN(personnels.A.B[ , .(Matricule, Année, Mois)], by = NULL)
   
   nombre.agents.cumulant.A.B <- uniqueN(personnels.A.B$Matricule)
   
-  personnels.A.B <- personnels.A.B[order(Année, Mois, Matricule)]
+  setkey(personnels.A.B, Matricule,Année, Mois)
 }
+},
+  "La détection des cumuls d'indemnités " %+% ident_prime %+% " et " %+% prime$prime_B %+% " n'a pas pu être réalisée.")
 
+
+essayer({
+  
 L <- length(get(K))
-
+  
 if (L < 6) {
   
   print(Tableau(c("Codes " %+% ident_prime,
@@ -108,11 +139,12 @@ if (L < 6) {
   cat("Codes " %+% ident_prime %+% " : ", paste(unlist(get(K)), collapse = " "))
   
 }
+}, "Le tableau des cumuls ne peut pas être généré.")
 
 env <- environment()
 
 sauvebase <- function(x, y) {
-  Sauv.base(file.path(chemin.dossier.bases, "Reglementation"),
+  Sauv.base(file.path(chemin.dossier.bases, prime$dossier),
   x,
   y,
   FALSE,
@@ -121,15 +153,63 @@ sauvebase <- function(x, y) {
   environment = env)
 }
 
-sauvebase("personnels.A.B" , "personnels.pfr.ifts")
-sauvebase("A.non.cat" , "PFR.non.catA")
-sauvebase("A.non.tit" , "PFR.non.tit")
+essayer({
+  sauvebase("personnels.A.B", "personnels." %+% tolower(ident_prime) %+% "." %+% tolower(prime$prime_B))
+  sauvebase("A.non.cat", ident_prime %+% ".non.cat" %+% prime$catégorie)
+  sauvebase("A.non.tit", ident_prime %+% ".non.tit")
+}, "Pas de sauvegarde des fichiers auxiliaires.")
+
+Lignes_A[ , A_ := TRUE]
+
+beneficiaires.A <- merge(Lignes_A, Lignes_B, all = TRUE)
+
+beneficiaires.A[ , Régime := if (all(is.na(A_))) { if (any(get(prime$prime_B))) "I" else NA } else { if (all(is.na(get(prime$prime_B)))) "P" else "C" },
+                   by = .(Matricule, Année, Mois)][ , `:=`(A_ = NULL, 
+                                                           B_ = NULL)]
+
+matricules.A <- unique(Lignes_A$Matricule)
+
+beneficiaires.A <- beneficiaires.A[Matricule %chin% matricules.A,
+                                       .(Agrégat = sum(Montant, na.rm = TRUE),
+                                         c = uniqueN(Mois[Régime == "C"]),
+                                         nb.mois = uniqueN(Mois),
+                                         Grade = Grade[1],
+                                         Mois,
+                                         Régime),
+                                       keyby= .(Matricule, Année),
+                                  ][ , 
+                                      .(Agrégat,
+                                        c,
+                                        Grade,
+                                        nb.mois,
+                                        Régime = {
+                                       
+                                       "IFTS " %+% uniqueN(Mois[Régime == "I"]) %+% " mois-PFR " %+% uniqueN(Mois[Régime == "P"]) %+% " mois" %+% "-Cumul " %+% c %+% " mois"
+                                     }),
+                                      keyby= .(Matricule, Année)]
+
+beneficiaires.A <- unique(beneficiaires.A)
+
+beneficiaires.A.Variation <- beneficiaires.A[ , 
+                                                  { 
+                                                    L <- length(Année)
+                                                    q <- Agrégat[L]/Agrégat[1] * nb.mois[1]/nb.mois[L]                   
+                                                    .(Années = paste(Année, collapse = ", "), 
+                                                      `Variation (%)` = round((q - 1) * 100, 1),
+                                                      `Moyenne géométrique annuelle(%)` = round((q^(1/(L - 1)) - 1) * 100, 1)) 
+                                                  }, by = "Matricule"]
+
+beneficiaires.A.Variation <- beneficiaires.A.Variation[`Variation (%)` != 0.00]
+
 
 list(Paie = Paie_A, 
      Lignes = Lignes_A, 
      personnels = personnels.A.B, 
      non.cat = A.non.cat, 
      mois = nombre.mois.cumuls, 
-     N = nombre.agents.cumulant.A.B)
+     N = nombre.agents.cumulant.A.B,
+     cumuls = beneficiaires.A,
+     variations = beneficiaires.A.Variation,
+     matricules = matricules.A)
 
 }
