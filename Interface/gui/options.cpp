c695185ea4472860743b2a9491d27b83398ef75c
Fabrice Nicol <fabrnicol@gmail.com>
2018-09-07 00:34:17 +0200

    amélioration onglet options

--- Interface/gui/options.cpp
+++ Interface/gui/options.cpp
@@ -1016,25 +1016,34 @@ processPage::processPage()
 
         const std::string &root = path_access (".").toStdString();
         int current_git_branch = system (std::string ("cd " + root + " && test \"$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)\" = \"master-jf\"").c_str());
+        int current_git_branch2 = 1;
+        if (current_git_branch  != 0) current_git_branch2 = system (std::string ("cd " + root + " && test \"$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)\" = \"dev\"").c_str());
 
         if (rapportEntier->isChecked())
             {
-                if (current_git_branch == 0)
+                if (current_git_branch == 0 || current_git_branch2 == 0)
                     {
                         // La branche courante est la branch Test
                         return;
                     }
 
-                int res = system (std::string ("cd " + root + " && git rev-parse --verify master-jf").c_str());
+                int res  = system (std::string ("cd " + root + " && git rev-parse --verify master-jf").c_str());
+                
+                int res2 = 1;
+                
+                if (res != 0) res2 = system (std::string ("cd " + root + " && git rev-parse --verify dev").c_str());
 
-                if (res != 0)
+                if (res != 0 && res2 != 0)
                     {
                         Q ("La branche Test n'est pas déployée.")
                         return;
                     }
 
                 Q ("Basculement vers la version Test.<br>Cela peut prendre une ou deux minutes.")
-                res = system (std::string ("cd " + root + " && git checkout -f master-jf").c_str());
+                if (res == 0)        
+                    res = system (std::string ("cd " + root + " && git checkout -f master-jf").c_str());
+                else // res2 = 0
+                    res = system (std::string ("cd " + root + " && git checkout -f dev").c_str());
 
                 if (res == 0)
                     {
@@ -1113,9 +1122,13 @@ processPage::processPage()
 
     // provisoire
     const std::string &root = path_access (".").toStdString();
-    int current_git_branch = system (std::string ("cd " + root + " && test \"$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)\" = \"master-jf\"").c_str());
-    rapportEntier->setChecked (current_git_branch == 0);
+    int current_git_branch = system(std::string ("cd " + root + " && test \"$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)\" = \"master-jf\"").c_str());
     
+    int current_git_branch2 = 1;
+    current_git_branch2 = system(std::string ("cd " + root + " && test \"$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)\" = \"dev\"").c_str());
+    
+    rapportEntier->setChecked(current_git_branch == 0 || current_git_branch2  == 0);
+        
     reinitialiser_prologue();
 }
 
