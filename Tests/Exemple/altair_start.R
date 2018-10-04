4fe5138457a292ab43cb153c43ea4b11ba5441c4
Fabrice Nicol <fabrnicol@gmail.com>
2018-10-05 01:32:09 +0200

    immature correction

--- Tests/Exemple/altair_start.R
+++ Tests/Exemple/altair_start.R
@@ -220,16 +220,16 @@ prime_PFI <- list(nom = "PFI",                      # Nom en majuscules
                   restreint_fonctionnaire = TRUE,   # fonctionnaires
                   catégorie = c("A", "B", "C"),     # toutes les catégories
                   dossier = "Reglementation")       # dossier de bases
-
-if (setOSWindows) séquentiel <- TRUE else {
-
-  system('cat /proc/meminfo > mem.txt')
-  mem <- data.table::fread("mem.txt", 
-               sep = ":", 
-               header = FALSE)[V1 == "MemAvailable", V2]
-  mem <- strtoi(unlist(strsplit(mem, " "))[1])
-  if (nrow(Paie) * ratio.memoire.ligne.parallele  > mem) séquentiel <- TRUE
-}
+#immature
+# if (setOSWindows) séquentiel <- TRUE else {
+# 
+#   system('cat /proc/meminfo > mem.txt')
+#   mem <- data.table::fread("mem.txt", 
+#                sep = ":", 
+#                header = FALSE)[V1 == "MemAvailable", V2]
+#   mem <- strtoi(unlist(strsplit(mem, " "))[1])
+#   if (nrow(Paie) * ratio.memoire.ligne.parallele  > mem) séquentiel <- TRUE
+# }
 
 
 scripts <- list(
