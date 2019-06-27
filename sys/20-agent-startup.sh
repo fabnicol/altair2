e4ce938412207ec8b8dbea10f8ea144b8cb729e0
Fabrice Nicol <fabrice.nicol@crtc.ccomptes.fr>
2019-06-27 13:42:11 +0200

    add sleep to plasma startup warning

--- sys/20-agent-startup.sh
+++ sys/20-agent-startup.sh
@@ -4,6 +4,8 @@
 # Uncomment the following lines to kill the agents
 # that were started at session startup.
 
+sleep 120s
+
 a=$(rc-status | grep ajuster_version | grep starting)
 
 if test "y$a" != "y"; then
